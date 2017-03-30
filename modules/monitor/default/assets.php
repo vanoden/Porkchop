e566786f08dceb3760345feb41e1f479796f49ca
Anthony Caravello <tony@caravello.us>
2017-03-30 15:26:45 -0400

    Improving layouts

--- modules/monitor/default/assets.php
+++ modules/monitor/default/assets.php
@@ -14,26 +14,27 @@
 </style>
 <table class="body" cellpadding="0" cellspacing="0">
 <tr><td colspan="4" class="title">Assets [<?=count($assets)?>]</td></tr>
-<tr><td class="label serialNumberLabel">Serial Number</td>
-	<td class="label zonesLabel">Zones</td>
-	<td class="label modelLabel">Model</td>
-	<td class="label nameLabel">Name</td>
+<tr><td class="label columnLabel columnLabelLeft serialNumberLabel">Serial Number</td>
+	<td class="label columnLabel zonesLabel">Zones</td>
+	<td class="label columnLabel modelLabel">Model</td>
+	<td class="label columnLabel columnLabelRight nameLabel">Name</td>
 </tr>
 <?	$greenbar = '';
 	foreach ($assets as $asset) {
 ?>
-<tr><td class="value <?=$greenbar?>"><a href="/_monitor/asset/<?=$asset->id?>"><?=$asset->code?></a></td>
+<tr><td class="value columnValue columnValueLeft <?=$greenbar?>"><a href="/_monitor/asset/<?=$asset->id?>"><?=$asset->code?></a></td>
 <?	app_log("Counting sensors",'debug',__FILE__,__LINE__); ?>
-	<td class="value <?=$greenbar?>"><?=count($asset->sensors())?></td>
+	<td class="value columnValue <?=$greenbar?>"><?=count($asset->sensors())?></td>
 <?	app_log("Showing product code",'debug',__FILE__,__LINE__); ?>
-	<td class="value <?=$greenbar?>"><?=$asset->product->code?></td>
+	<td class="value columnValue <?=$greenbar?>"><?=$asset->product->code?></td>
 <?	app_log("Showing asset name",'debug',__FILE__,__LINE__); ?>
-	<td class="value <?=$greenbar?>"><?=$asset->name?></td>
+	<td class="value columnValue columnValueRight <?=$greenbar?>"><?=$asset->name?></td>
 <?	app_log("Finished line",'debug',__FILE__,__LINE__); ?>
 </tr>
 <?
 		if ($greenbar) $greenbar = '';
 		else $greenbar = "greenbar";
 	} ?>
+<tr><td colspan="4" class="table_footer"></td></tr>
 </table>
 <? app_log("template completed"); ?>
--- modules/monitor/default/collections.php
+++ modules/monitor/default/collections.php
@@ -15,8 +15,11 @@
 	.collectionDeleteColumn {
 		width: 35px;
 	}
-	.value {
-		font-size: 12px;
+	#btn_new_collection {
+		width: 145px;
+		position: absolute;
+		top: 6px;
+		left: 260px;
 	}
 </style>
 <script language="Javascript">
@@ -49,32 +52,33 @@
 <form name="collectionsForm" method="post">
 <input id="delete_collection" type="hidden" name="delete_collection" value=""/>
 <table class="body monitorCollectionsBody" cellpadding="0" cellspacing="0">
-<tr><td colspan="4" class="title">Jobs</td></tr>
+<tr><td colspan="4" class="title">Collections</td></tr>
 <tr><td style="text-align: center" colspan="4">
-	<input type="button" name="btn_new" class="button" onclick="newCollection()" value="New Collection" />
+	<input type="button" id="btn_new_collection" name="btn_new" class="button" onclick="newCollection()" value="New Collection" />
 </td>
 </tr>
-<tr><td class="label columnLabel collectionNameColumn">Name</td>
+<tr><td class="label columnLabel columnLabelLeft collectionNameColumn">Name</td>
 	<td class="label columnLabel collectionCustomerColumn">Customer</td>
 	<td class="label columnLabel collectionStartedColumn">Started</td>
 	<td class="label columnLabel collectionFinishedColumn">Finished</td>
-	<td class="label columnLabel collectionDeleteColumn">Delete</td>
+	<td class="label columnLabel columnLabelRight collectionDeleteColumn">Delete</td>
 </tr>
 <?	foreach ($collections as $collection) {
 		$name = $collection->metadata('name');
 		if (! $name) $name = "[none]";
 		
 ?>
-<tr><td class="value columnValue collectionNameColumn<?=$greenbar?>"><a href="/_monitor/dashboard/<?=$collection->code?>" id="Collection[<?=$collection->id?>]"><?=$name?></a></td>
+<tr><td class="value columnValue columnValueLeft collectionNameColumn<?=$greenbar?>"><a href="/_monitor/dashboard/<?=$collection->code?>" id="Collection[<?=$collection->id?>]"><?=$name?></a></td>
 	<td class="value columnValue collectionCustomerColumn<?=$greenbar?>"><?=$collection->metadata('customer')?></td>
 	<td class="value columnValue collectionStartedColumn<?=$greenbar?>"><?=date("m/d/y H:m",$collection->timestamp_start)?></td>
 	<td class="value columnValue collectionFinishedColumn<?=$greenbar?>"><?=date("m/d/y H:m",$collection->timestamp_end)?></td>
-	<td class="value columnValue collectionDeleteColumn<?=$greenbar?>" style="text-align: center"><input type="button" style="padding-left: 2px; padding-right: 2px; padding-top: 0px; height: 18px; font-weight: bold" name="delete_collection" value="x" onclick="deleteCollection(<?=$collection->id?>)" /></td>
+	<td class="value columnValue columnValueRight collectionDeleteColumn<?=$greenbar?>" style="text-align: center"><input type="button" style="padding-left: 2px; padding-right: 2px; padding-top: 0px; height: 18px; font-weight: bold" name="delete_collection" value="x" onclick="deleteCollection(<?=$collection->id?>)" /></td>
 </tr>
 <?
 		if ($greenbar) $greenbar = "";
 		else $greenbar = " greenbar";
 	}
 ?>
+<tr><td colspan="5" class="table_footer"></td></tr>
 </table>
 </form>
