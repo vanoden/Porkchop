9fe22df1fbb9c721a12bd97a88a815bad4c83d1d
Anthony Caravello <tony@caravello.us>
2017-03-31 15:28:24 -0400

    Working on product management

---
+++ classes/Media/ImageList.php
@@ -0,0 +1,14 @@
+<?
+	namespace Media;
+
+	class ImageList {
+		public $error;
+		public $count;
+
+		public function find($parameters = array()) {
+			$parameters['type'] = 'document';
+			$itemlist = new ItemList();
+			return $itemlist::find($parameters);
+		}
+	}
+?>
