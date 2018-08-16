3f4c23fc11b5631f076a4684e2ed77bf9b8b9f42
Anthony Caravello <tony@caravello.us>
2018-08-15 23:36:01 -0400

    Forgot Password Stuff

---
+++ modules/register/default/password_token_sent.php
@@ -0,0 +1,5 @@
+<div class="body">
+<span>Thank You</span>
+<p>If your email address was recognized, you will receive an email with a link to reset your password in the next few minutes.</p>
+<p>Please make sure no-reply@spectrosinstruments.com emails are not moved to your spam folder.</p>
+<p>If you do not receive an email, contact us via <a href="/contact_us.html">our contact form</a> or email directly to our service account, <a href="mailto:service@spectrosinstruments.com">service@spectrosinstruments.com</a>.</p>
