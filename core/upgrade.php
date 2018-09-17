f74bf2375cde6ec6312cd134ae0e7a8f7b2bd18d
Anthony Caravello <tony@caravello.us>
2018-09-17 12:23:01 -0400

    New version

--- core/upgrade.php
+++ core/upgrade.php
@@ -44,7 +44,7 @@
 	header("Expires: 0");
 	header("Cache-Control: no-cache, must-revalidate");
 
-	install_log("Upgrading site to 201808050715");
+	install_log("Upgrading site to 201809171222");
 
 	# Get version.txt
 	if (file_exists(HTML."/version.txt")) {
@@ -136,7 +136,7 @@
 #	if ($class->version() != 0) install_fail("Version 1 Required");
 	$class = new \Engineering\Schema();
 	install_log("Engineering::Schema: version ".$class->version());
-	if ($class->version() != 1) install_fail("Version 1 Required");
+	if ($class->version() != 2) install_fail("Version 1 Required");
 
 	###################################################
 	### Initialize Session							###
@@ -232,6 +232,8 @@
 		array("engineering","release"),
 		array("engineering","products"),
 		array("engineering","product"),
+		array("engineering","projects"),
+		array("engineering","project"),
 	);
 
 	install_log("Add new template settings");
