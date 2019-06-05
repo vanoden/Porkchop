8fe4f6af455dd90a9fae589775fe037649679e26
Anthony Caravello <tony@caravello.us>
2019-06-05 13:48:52 -0400

    Added bar code scanning

--- modules/barcode/default/read.php
+++ modules/barcode/default/read.php
@@ -4,5 +4,5 @@
 <form action="/_barcode/read" method="post" enctype="multipart/form-data">
 	Select image to upload:
 	<input type="file" name="barcode" id="barcode">
-	<input type="submit" value="Upload Image" name="submit">
+	<input type="submit" value="Upload Image" name="method">
 </form>
