72834090f5bda6da88e2f35c79f05d4daabebdaa
Anthony Caravello <tony@caravello.us>
2019-06-11 10:22:45 -0400

    Added admin home page

---
+++ modules/admin/default/home.php
@@ -0,0 +1,4 @@
+<p>This page is for authorized administrators of <?=$GLOBALS['_SESSION_']->company->name?> ONLY!</p>
+<p><strong>All use of these tools is monitored and logged.</strong></p>
+<br>
+<p>Select the menu item on the left to use administration tools.</p>
