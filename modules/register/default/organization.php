2a5597c133716fa309572c074b2ef3a4eeb636fa
Kevin Hinds <khinds10.work@gmail.com>
2019-06-12 13:25:05 -0400

    remove the AJAX, Tony fixed the DB

--- modules/register/default/organization.php
+++ modules/register/default/organization.php
@@ -1,50 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-<style>
-	.loading-spinner {
-		-webkit-animation-name: spin;
-		-webkit-animation-duration: 1000ms;
-		-webkit-animation-iteration-count: infinite;
-		-webkit-animation-timing-function: linear;
-		-moz-animation-name: spin;
-		-moz-animation-duration: 1000ms;
-		-moz-animation-iteration-count: infinite;
-		-moz-animation-timing-function: linear;
-		-ms-animation-name: spin;
-		-ms-animation-duration: 1000ms;
-		-ms-animation-iteration-count: infinite;
-		-ms-animation-timing-function: linear;
-		animation-name: spin;
-		animation-duration: 1000ms;
-		animation-iteration-count: infinite;
-		animation-timing-function: linear;
-	}
-	@-ms-keyframes spin {
-		from { -ms-transform: rotate(0deg); }
-		to { -ms-transform: rotate(360deg); }
-	}
-	@-moz-keyframes spin {
-		from { -moz-transform: rotate(0deg); }
-		to { -moz-transform: rotate(360deg); }
-	}
-	@-webkit-keyframes spin {
-		from { -webkit-transform: rotate(0deg); }
-		to { -webkit-transform: rotate(360deg); }
-	}
-	@keyframes spin {
-		from {
-		    transform:rotate(0deg);
-		}
-		to {
-		    transform:rotate(360deg);
-		}
-	}
-</style>
-
-
 <h2>Organization Details</h2>
 <form name="orgDetails" method="POST">
 <input type="hidden" name="organization_id" value="<?=$organization->id?>"/>
@@ -57,21 +10,6 @@
 <?  } ?>
 <div class="form_instruction">Make changes and click 'Apply' to complete.</div>
 
-<script>
-    // @TODO AJAX to get the slow responses on last active fixed for now, 
-    //      prob going to need to convert the fields over to timestamps to index the column in the best way
-    $(document).ready(function(){
-        $('.active-date-lookup').each(function(){
-            customerId = this.id.split('customer-id-active-').pop();
-            currentElement = this;
-            $.get( "/_register/api", { __format: "json", memberId: customerId, method: 'getMemberLastActive' }).done(function( data ) {
-                data = $.parseJSON(data);
-                $('#customer-id-loading-' + data.memberId).hide();    
-                $('#customer-id-active-' + data.memberId).html(data.lastActive);
-            });
-        });
-    });
-</script>
 <!--	Start First Row-->
 <div class="tableBody min-tablet marginTop_20">
 	<div class="tableRowHeader">
@@ -148,8 +86,7 @@
 			<?=$member->status?>
 		</div>
 		<div class="tableCell">
-		    <i id="customer-id-loading-<?=$member->id?>" class="fa fa-refresh loading-spinner" aria-hidden="true"></i>
-		    <span id="customer-id-active-<?=$member->id?>" class="active-date-lookup"></span>
+			<?=$member->last_active()?>
 		</div>
 	</div>
 <?	} ?>
