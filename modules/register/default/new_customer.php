248531d14cc15ba041d031188e35efb26b3dd714
Anthony Caravello <tony@caravello.us>
2019-06-13 09:38:08 -0400

    Changed 'Register Now' button to point to new registration in login page, modified new registration code to use existing Asset object

--- modules/register/default/new_customer.php
+++ modules/register/default/new_customer.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<script src="/js/monitor.js"></script>
 <script type="text/javascript">
     // validate password and submit if ok to go
     function submitForm() {
@@ -21,23 +22,66 @@
        });
    });
    
-   // make sure the serial number is valid
-   function checkSerial() {
-    var serialInput = $("#serial_number");
-    var serialNumberMessage = $("#serial_number_message");
-    var serialNumberMessageOK = $("#serial_number_message_ok");
-    $.post( "/_register/api", { __format: "json", serialNumber: serialInput.val(), method: 'shipmentFindBySerial' }).done(function( data ) {
-        if (data.ID) {
-            serialInput.attr('style', 'border: solid 2px green;');
-            serialNumberMessage.hide();
-            serialNumberMessageOK.show();
-        } else {
-            serialInput.attr('style', 'border: solid 2px red;');
-            serialNumberMessage.show();
-            serialNumberMessageOK.hide();
-        }
-    });
-   }
+	function checkProduct() {
+		var productElem = document.getElementById('product_id');
+		var serialNumber = document.getElementById('serial_number');
+		var serialMessage = document.getElementById('serial_number_message');
+		serialNumber.focus();
+		serialNumber.style.border = '1px solid gray';
+		if (productElem.selectedIndex > 0) {
+			if (document.getElementById('serial_number').value.length > 0) {
+				serialMessage.display = 'none';
+			}
+			return true;
+		}
+		else {
+			serialMessage.innerHTML = 'Select a product first';
+			serialMessage.style.display = 'block';
+			productElem.focus();
+			return false;
+		}
+	}
+
+	// make sure the serial number is valid
+	function checkSerial() {
+		var productInput = document.getElementById('product_id');
+		checkProduct();
+		var productID = productInput.options[productInput.selectedIndex].value;
+
+		var serialInput = document.getElementById('serial_number');
+		var serialNumberMessage = document.getElementById('serial_number_message');
+		var serialNumberMessageOK = document.getElementById('serial_number_message_ok');
+
+		if (serialInput.value.length < 1) {
+			return true;
+		}
+		var code = serialInput.value;
+		var asset = Object.create(Asset);
+
+		if (asset.get(code)) {
+			if (asset.product.id == productID) {
+				serialInput.style.border = 'solid 2px green';
+				serialNumberMessage.style.display = 'none';
+				serialNumberMessageOK.innerHTML = 'Serial number has been found, thank you for providing!';
+				serialNumberMessageOK.style.display = 'block';
+				return true;
+			}
+			else {
+				serialInput.style.border = 'solid 2px red';
+				serialNumberMessage.innerHTML = 'Product not found with that serial number';
+				serialNumberMessage.style.display = 'block';
+				serialNumberMessageOK.style.display = 'none';
+				return false;
+			}
+		}
+		else {
+			serialInput.style.border = 'solid 2px red';
+			serialNumberMessage.innerHTML = 'Serial number not found in our system';
+			serialNumberMessage.style.display = 'block';
+			serialNumberMessageOK.style.display = 'none';
+			return false;
+		}
+	}
 </script>
 <style>
    .long-field {
@@ -87,17 +131,17 @@
             <input id="has_item_checkbox" type="checkbox" name="reseller" value="yes"> Already have a device you would like to register?<br/>
          </div>
          <div id="product_details" style="display:none;">
-            <span class="label"><i class="fa fa-barcode" aria-hidden="true"></i> Serial #</span>
-            <input type="text" id="serial_number" class="long-field" name="serial_number" placeholder="Serial Number" onblur="checkSerial()">
-            <div id="serial_number_message" style="color:red; display:none;">Serial number not found in our system<br/><br/></div>
-            <div id="serial_number_message_ok" style="color:green; display:none;">Serial number has been found, thank you for providing!<br/><br/></div>
-            <span class="label"><i class="fa fa-cog" aria-hidden="true"></i> Product:</span>
-            <select id="product_id" name="product_id" class="value input collectionField">
+            <span class="label" style="display: block"><i class="fa fa-cog" aria-hidden="true"></i> Product:</span>
+            <select id="product_id" name="product_id" class="value input collectionField" style="display: block" onchange="document.getElementById('serial_number_message').style.display = 'none';">
                <option value=""<? if ($product == $selectedProduct) print " selected"; ?>>---</option>
                <?php	foreach ($productsAvailable as $product) { ?>
                     <option value="<?=$product->id?>"<? if ($product->id == $selectedProduct) print " selected"; ?>><?=$product->code?> - <?=$product->description?></option>
                <?php	} ?>
             </select>
+            <span class="label"><i class="fa fa-barcode" aria-hidden="true"></i> Serial #</span>
+            <input type="text" id="serial_number" class="long-field" name="serial_number" placeholder="Serial Number" onfocus="checkProduct();" onblur="checkSerial()">
+            <div id="serial_number_message" style="color:red; display:none;">Serial number not found in our system<br/><br/></div>
+            <div id="serial_number_message_ok" style="color:green; display:none;">Serial number has been found, thank you for providing!<br/><br/></div>
          </div>
          <br/>
 
