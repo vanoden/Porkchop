f359dfb471f97468c6610acca0db4f0e06a2f5b9
Anthony Caravello <tony@caravello.us>
2019-10-22 10:24:16 -0400

    Finished adding calls to Engineering API

--- classes/Engineering/TaskList.php
+++ classes/Engineering/TaskList.php
@@ -13,37 +13,31 @@
 		 * @param array $parameters
 		 */
 		public function find($parameters = array()) {
-		
+			if (isset($parameters['searchTerm'])) return $this->search($parameters);
+
 			$find_objects_query = "
 				SELECT	id
 				FROM	engineering_tasks
 				WHERE	id = id
 			";
 
-            // if search term, then constrain by that
-            if ($parameters['searchTerm']) {            
-                $find_objects_query = "
-                SELECT	`id`
-                FROM	`engineering_tasks`
-                    WHERE `code` LIKE '%".$parameters['searchTerm']."%' 
-                    OR `title` LIKE '%".$parameters['searchTerm']."%' 
-                    OR `description` LIKE '%".$parameters['searchTerm']."%'
-                    OR `location` LIKE '%".$parameters['searchTerm']."%' ";
-            }
-
+			$bind_params = array();
 			if (isset($parameters['project_id']) && is_numeric($parameters['project_id'])) {
 				$find_objects_query .= "
-				AND		project_id = ".$parameters['project_id'];
+				AND		project_id = ?";
+				array_push($bind_params,$parameters['project_id']);
 			}
 
 			if (isset($parameters['assigned_id']) && is_numeric($parameters['assigned_id'])) {
 				$find_objects_query .= "
-				AND		assigned_id = ".$parameters['assigned_id'];
+				AND		assigned_id = ?";
+				array_push($bind_params,$parameters['assigned_id']);
 			}
 
 			if (isset($parameters['release_id']) && is_numeric($parameters['release_id'])) {
 				$find_objects_query .= "
 				AND		release_id = ".$parameters['release_id'];
+				array_push($bind_params,$parameters['release_id']);
 			}
 
 			if (isset($parameters['status']) && !empty($parameters['status'])) {
@@ -57,12 +51,17 @@
 							$icount ++;
 							$find_objects_query .= "'".$status."'";
 						}
+						else {
+							$this->_error = "Invalid status";
+							return null;
+						}
 					}
 					$find_objects_query .= ")";
 				}
 				else {
 					$find_objects_query .= "
-				AND		status = ".$GLOBALS['_database']->qstr($parameters['status'],get_magic_quotes_gpc());
+				AND		status = ?";
+				array_push($bind_params,$parameters['status']);
 				}
 			}
 			else {
@@ -75,9 +74,8 @@
 				LIMIT ".$parameters['_limit'];
 			}
 
-			$rs = $GLOBALS['_database']->Execute(
-				$find_objects_query
-			);
+			query_log($find_objects_query,$bind_params);
+			$rs = $GLOBALS['_database']->Execute($find_objects_query,$bind_params);
 
 			if (! $rs) {
 				$this->_error = "SQL Error in Engineering::TaskList::find(): ".$GLOBALS['_database']->ErrorMsg();
@@ -94,6 +92,87 @@
 			return $tasks;
 		}
 
+		public function search($parameters) {
+			$search_term = null;
+			if (preg_match('/^[\w\-\.\_\s\*]+$/')) $search_term = $parameters['searchTerm'];
+
+			$find_objects_query = "
+				SELECT	`id`
+				FROM	`engineering_tasks`
+				WHERE	(`code` LIKE '%".$search_term."%' 
+				OR		`title` LIKE '%".$search_term."%' 
+				OR		`description` LIKE '%".$search_term."%'
+				OR		`location` LIKE '%".$search_term."%')";
+
+			$bind_params = array();
+			if (isset($parameters['project_id']) && is_numeric($parameters['project_id'])) {
+				$find_objects_query .= "
+				AND		project_id = ?";
+				array_push($bind_params,$parameters['project_id']);
+			}
+
+			if (isset($parameters['assigned_id']) && is_numeric($parameters['assigned_id'])) {
+				$find_objects_query .= "
+				AND		assigned_id = ?";
+				array_push($bind_params,$parameters['assigned_id']);
+			}
+
+			if (isset($parameters['release_id']) && is_numeric($parameters['release_id'])) {
+				$find_objects_query .= "
+				AND		release_id = ".$parameters['release_id'];
+				array_push($bind_params,$parameters['release_id']);
+			}
+
+			if (isset($parameters['status']) && !empty($parameters['status'])) {
+				if (is_array($parameters['status'])) {
+					$icount = 0;
+					$find_objects_query .= "
+				AND		status IN (";
+					foreach ($parameters['status'] as $status) {
+						if (preg_match('/^[\w\-\_\.\s]+$/',$status)) {
+							if ($icount > 0) $find_objects_query .= ",";
+							$icount ++;
+							$find_objects_query .= "'".$status."'";
+						}
+						else {
+							$this->_error = "Invalid status";
+							return null;
+						}
+					}
+					$find_objects_query .= ")";
+				}
+				else {
+					$find_objects_query .= "
+				AND		status = ?";
+				array_push($bind_params,$parameters['status']);
+				}
+			}
+			else {
+				$find_objects_query .= "
+				AND		status NOT IN ('CANCELLED')";
+			}
+
+			if (isset($parameters['_limit']) && is_numeric($parameters['_limit'])) {
+				$find_objects_query .= "
+				LIMIT ".$parameters['_limit'];
+			}
+			query_log($find_objects_query,$bind_params);
+			$rs = $GLOBALS['_database']->Execute($find_objects_query,$bind_params);
+
+			if (! $rs) {
+				$this->_error = "SQL Error in Engineering::TaskList::search(): ".$GLOBALS['_database']->ErrorMsg();
+				return null;
+			}
+
+			$tasks = array();
+
+			while (list($id) = $rs->FetchRow()) {
+				$task = new Task($id);
+				array_push($tasks,$task);
+			}
+
+			return $tasks;
+		}
 		public function error() {
 			return $this->_error;
 		}
