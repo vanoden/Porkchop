0093ebdbbd545ec8660851975a7baf47cef17785
Anthony Caravello <tony@caravello.us>
2019-10-30 17:35:06 -0400

    Added Geography Schema

---
+++ classes/Geography/Schema.php
@@ -0,0 +1,154 @@
+<?php
+	namespace Geography;
+
+	class Schema {
+		public $errno;
+		public $error;
+		public $module = "geography";
+
+		public function __construct() {
+			$this->upgrade();
+		}
+	
+		public function version() {
+			# See if Schema is Available
+			$schema_list = $GLOBALS['_database']->MetaTables();
+			$info_table = strtolower($this->module)."__info";
+
+			if (! in_array($info_table,$schema_list)) {
+				# Create __info table
+				$create_table_query = "
+					CREATE TABLE `$info_table` (
+						label   varchar(100) not null primary key,
+						value   varchar(255)
+					)
+				";
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating info table in ".$this->module."Schema::version: ".$GLOBALS['_database']->ErrorMsg();
+					return null;
+				}
+			}
+
+			# Check Current Schema Version
+			$get_version_query = "
+				SELECT  value
+				FROM    `$info_table`
+				WHERE   label = 'schema_version'
+			";
+
+			$rs = $GLOBALS['_database']->Execute($get_version_query);
+			if (! $rs) {
+				$this->error = "SQL Error in ".$this->module."::version: ".$GLOBALS['_database']->ErrorMsg();
+				return null;
+			}
+
+			list($version) = $rs->FetchRow();
+			if (! $version) $version = 0;
+			return $version;
+		}
+		
+		public function upgrade() {
+			$this->error = '';
+			$info_table = strtolower($this->module)."__info";
+
+			# See if Schema is Available
+			$schema_list = $GLOBALS['_database']->MetaTables();
+
+			if (! in_array($info_table,$schema_list)) {
+				# Create company__info table
+				$create_table_query = "
+					CREATE TABLE `$info_table` (
+						label	varchar(100) not null primary key,
+						value	varchar(255)
+					)
+				";
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating info table in ".$this->module."Schema::upgrade: ".$GLOBALS['_database']->ErrorMsg();
+					return null;
+				}
+			}
+
+			# Check Current Schema Version
+			$get_version_query = "
+				SELECT	value
+				FROM	`$info_table`
+				WHERE	label = 'schema_version'
+			";
+
+			$rs = $GLOBALS['_database']->Execute($get_version_query);
+			if (! $rs) {
+				$this->error = "SQL Error in ".$this->module."Schema::upgrade: ".$GLOBALS['_database']->ErrorMsg();
+				return null;
+			}
+
+			list($current_schema_version) = $rs->FetchRow();
+			if ($current_schema_version < 1) {
+				app_log("Upgrading schema to version 1",'notice',__FILE__,__LINE__);
+
+				# Start Transaction
+				if (! $GLOBALS['_database']->BeginTrans())
+					app_log("Transactions not supported",'warning',__FILE__,__LINE__);
+
+				# Geography Countries
+				$create_table_query = "
+					CREATE TABLE IF NOT EXISTS `geography_country` (
+						id INT(11) NOT NULL AUTO_INCREMENT,
+						name VARCHAR(255) NOT NULL,
+						abbreviation VARCHAR(100),
+						view_order INT(11) NOT NULL DEFAULT 500,
+						PRIMARY KEY `pk_id` (`id`),
+						UNIQUE KEY `uk_name` (`name`),
+						UNIQUE KEY `uk_abbrev` (`abbreviation`)
+					)
+				";
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating geography_country table in Geography::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return false;
+				}
+
+				# Collection of Geography Regions
+				$create_table_query = "
+					CREATE TABLE IF NOT EXISTS `geography_regions` (
+						id INT(11) NOT NULL AUTO_INCREMENT,
+						code varchar(255) NOT NULL,
+						country_id INT(11) NOT NULL,
+						name varchar(255) NOT NULL,
+						abbreviation varchar(100) NOT NULL,
+						PRIMARY KEY `pk_id` (`id`),
+						UNIQUE KEY `uk_code` (`code`),
+						UNIQUE KEY `uk_name` (`country_id`,`name`),
+						FOREIGN KEY `fk_country` (`country_id`) REFERENCES `geography_country` (`id`)
+					)
+				";
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating geography_regions table in Geography::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return false;
+				}
+
+				$current_schema_version = 1;
+				$update_schema_version = "
+					INSERT
+					INTO    ".$this->module."__info
+					VALUES  ('schema_version',$current_schema_version)
+					ON DUPLICATE KEY UPDATE
+						value = $current_schema_version
+				";
+				$GLOBALS['_database']->Execute($update_schema_version);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error in Geography::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return undef;
+				}
+				$GLOBALS['_database']->CommitTrans();
+			}
+		}
+	}
