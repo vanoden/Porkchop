79abef157318603429be22de8601bdc2b76662ec
Anthony Caravello <tony@caravello.us>
2019-11-13 09:49:17 -0500

    Added vendors to product schema

--- classes/Product/Schema.php
+++ classes/Product/Schema.php
@@ -4,6 +4,7 @@
 	class Schema {
 		public $error;
 		public $errno;
+		private $module = 'product';
 
 		public function __construct() {
 			$this->upgrade();
@@ -148,14 +149,115 @@
 				$current_schema_version = 1;
 				$update_schema_version = "
 					INSERT
-					INTO	product__info
+					INTO	".$this->module."__info
 					VALUES	('schema_version',$current_schema_version)
 					ON DUPLICATE KEY UPDATE
 						value = $current_schema_version
 				";
 				$GLOBALS['_database']->Execute($update_schema_version);
 				if ($GLOBALS['_database']->ErrorMsg()) {
-					$this->error = "SQL Error in ProductInit::schema_manager: ".$GLOBALS['_database']->ErrorMsg();
+					$this->error = "SQL Error in Product::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return 0;
+				}
+				$GLOBALS['_database']->CommitTrans();
+			}
+			if ($current_schema_version < 2) {
+				app_log("Upgrading schema to version 2",'notice',__FILE__,__LINE__);
+
+				# Start Transaction
+				if (! $GLOBALS['_database']->BeginTrans())
+					app_log("Transactions not supported",'warning',__FILE__,__LINE__);
+
+				$create_table_query = "
+					CREATE TABLE IF NOT EXISTS `product_vendors` (
+						`id` 			int(11) NOT NULL AUTO_INCREMENT,
+						`code` 			varchar(45) NOT NULL,
+						`name`			varchar(255),
+						`account_number`	varchar(255),
+						`status`		enum('ACTIVE','HIDDEN','DELETED') NOT NULL DEFAULT 'ACTIVE',
+						`notes`			text,
+						PRIMARY KEY `pk_vendor_id` (`id`),
+						UNIQUE KEY `uk_vendor_code` (`code`)
+					)
+				";
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating product_vendors table in Product::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return null;
+				}
+
+				$create_table_query = "
+					CREATE TABLE IF NOT EXISTS `product_vendor_locations` (
+						`vendor_id`		int(11) NOT NULL,
+						`location_id` 	int(11) NOT NULL,
+						PRIMARY KEY `pk_vendor_location` (`vendor_id`,`location_id`),
+						FOREIGN KEY `fk_vendor_id` (`vendor_id`) REFERENCES `product_vendors` (`id`),
+						FOREIGN KEY `fk_location_id` (`location_id`) REFERENCES `register_locations` (`id`)
+					)
+				";
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating product_vendor_locations table in Product::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return null;
+				}
+
+				$create_table_query = "
+					CREATE TABLE IF NOT EXISTS `product_vendor_items` (
+						`vendor_id`		int(11) NOT NULL,
+						`product_id`	int(11) NOT NULL,
+						`cost`			decimal(8,2) NOT NULL DEFAULT 0,
+						`delivery_time`	int(11) NOT NULL DEFAULT 0,
+						`minimum_order`	int(5) NOT NULL DEFAULT 0,
+						`vendor_sku`	varchar(255),
+						UNIQUE KEY `UK_VENDOR_ITEM` (`vendor_id`,`product_id`),
+						KEY `IDX_PRODUCT` (`product_id`),
+						FOREIGN KEY `FK_PRODUCT_ID` (`product_id`) REFERENCES `product_products` (`id`),
+						FOREIGN KEY `FK_VENDOR_ID` (`vendor_id`) REFERENCES `product_vendors` (`id`)
+					)
+				";
+
+				$GLOBALS['_database']->Execute($create_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error creating product_vendor_items table in Product::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return 0;
+				}
+
+				$alter_table_query = "
+					ALTER TABLE `product_products`
+					ADD 	`on_hand`			decimal(10,2) NOT NULL DEFAULT 0,
+					ADD 	`default_vendor`	INT(11),
+					ADD		`min_quantity`		decimal(10,2) NOT NULL DEFAULT 0,
+					ADD		`max_quantity` 		decimal(10,2),
+					ADD		`total_purchased`	decimal(10,2) NOT NULL DEFAULT 0,
+					ADD		`total_cost`		decimal(10,2) NOT NULL DEFAULT 0
+				";
+				$GLOBALS['_database']->Execute($alter_table_query);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error altering product_products table in Product::Schema::upgrade(): ".$GLOBALS['_database']->ErrorMsg();
+					app_log($this->error,'error',__FILE__,__LINE__);
+					$GLOBALS['_database']->RollbackTrans();
+					return 0;
+				}
+
+				$current_schema_version = 2;
+				$update_schema_version = "
+					INSERT
+					INTO	".$this->module."__info
+					VALUES	('schema_version',$current_schema_version)
+					ON DUPLICATE KEY UPDATE
+						value = $current_schema_version
+				";
+				$GLOBALS['_database']->Execute($update_schema_version);
+				if ($GLOBALS['_database']->ErrorMsg()) {
+					$this->error = "SQL Error in Product::Schema::upgrade() ".$GLOBALS['_database']->ErrorMsg();
 					app_log($this->error,'error',__FILE__,__LINE__);
 					$GLOBALS['_database']->RollbackTrans();
 					return 0;
