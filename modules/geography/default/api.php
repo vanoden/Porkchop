4912dafa30f5217571491f445de4d2a698702a76
Anthony Caravello <tony@caravello.us>
2019-12-03 17:52:10 -0500

    Updated geopgraphy api

--- modules/geography/default/api.php
+++ modules/geography/default/api.php
@@ -8,59 +8,36 @@
 			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
 		</div>
 		</form>
-		<form method="post" action="<?=PATH?>/_build/api" name="addProduct">
-		<input type="hidden" name="method" value="addProduct">
+		<form method="post" action="<?=PATH?>/_geography/api" name="addCountry">
+		<input type="hidden" name="method" value="addCountry">
 		<div class="apiMethod">
-			<div class="h3 apiMethodTitle">addProduct</div>
+			<div class="h3 apiMethodTitle">addCountry</div>
+			<div class="apiParameter">
+				<span class="label apiLabel">name</span>
+				<input type="text" name="code" class="value input apiInput"/>
+			</div>
+			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
+		</div>
+		</form>
+		<form method="post" action="<?=PATH?>/_geography/api" name="updateCountry">
+		<input type="hidden" name="method" value="updateCountry">
+		<div class="apiMethod">
+			<div class="h3 apiMethodTitle">updateCountry</div>
 			<div class="apiParameter">
 				<span class="label apiLabel">name</span>
 				<input type="text" name="name" class="value input apiInput"/>
 			</div>
 			<div class="apiParameter">
-				<span class="label apiLabel">workspace</span>
-				<input type="text" name="workspace" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">major_version</span>
-				<input type="text" name="major_version" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">minor_version</span>
-				<input type="text" name="minor_version" class="value input apiInput"/>
+				<span class="label apiLabel">status</span>
+				<input type="text" name="status" class="value input apiInput"/>
 			</div>
 			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
 		</div>
 		</form>
-		<form method="post" action="<?=PATH?>/_build/api" name="updateProduct">
-		<input type="hidden" name="method" value="updateProduct">
+		<form method="post" action="<?=PATH?>/_geography/api" name="getCountry">
+		<input type="hidden" name="method" value="getCountry">
 		<div class="apiMethod">
-			<div class="h3 apiMethodTitle">updateProduct</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">workspace</span>
-				<input type="text" name="workspace" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">major_version</span>
-				<input type="text" name="major_version" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">minor_version</span>
-				<input type="text" name="minor_version" class="value input apiInput"/>
-			</div>
-			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
-		</div>
-		</form>
-		<form method="post" action="<?=PATH?>/_build/api" name="findProducts">
-		<input type="hidden" name="method" value="findProducts">
-		<div class="apiMethod">
-			<div class="h3 apiMethodTitle">findProducts</div>
-			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
-		</div>
-		</form>
-		<form method="post" action="<?=PATH?>/_build/api" name="getProduct">
-		<input type="hidden" name="method" value="getProduct">
-		<div class="apiMethod">
-			<div class="h3 apiMethodTitle">getProduct</div>
+			<div class="h3 apiMethodTitle">getCountry</div>
 			<div class="apiParameter">
 				<span class="label apiLabel">name</span>
 				<input type="text" name="name" class="value input apiInput"/>
@@ -68,44 +45,65 @@
 			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
 		</div>
 		</form>
-		<form method="post" action="<?=PATH?>/_build/api" name="addVersion">
-		<input type="hidden" name="method" value="addVersion">
+		<form method="post" action="<?=PATH?>/_geography/api" name="findCountries">
+		<input type="hidden" name="method" value="findCountries">
 		<div class="apiMethod">
-			<div class="h3 apiMethodTitle">addVersion</div>
+			<div class="h3 apiMethodTitle">findCountries</div>
+			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
+		</div>
+		</form>
+		<form method="post" action="<?=PATH?>/_geography/api" name="addProvince">
+		<input type="hidden" name="method" value="addProvince">
+		<div class="apiMethod">
+			<div class="h3 apiMethodTitle">addProvince</div>
 			<div class="apiParameter">
-				<span class="label apiLabel">product</span>
-				<input type="text" name="product" class="value input apiInput"/>
+				<span class="label apiLabel">country_id</span>
+				<input type="text" name="country_id" class="value input apiInput"/>
 			</div>
 			<div class="apiParameter">
-				<span class="label apiLabel">number</span>
-				<input type="text" name="number" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">status</span>
-				<input type="text" name="status" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">tarball</span>
-				<input type="text" name="tarball" class="value input apiInput"/>
-			</div>
-			<div class="apiParameter">
-				<span class="label apiLabel">message</span>
-				<input type="text" name="message" class="value input apiInput"/>
+				<span class="label apiLabel">name</span>
+				<input type="text" name="name" class="value input apiInput"/>
 			</div>
 			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
 		</div>
 		</form>
-		<form method="post" action="<?=PATH?>/_build/api" name="findVersions">
-		<input type="hidden" name="method" value="findVersions">
+		<form method="post" action="<?=PATH?>/_geography/api" name="updateProvince">
+		<input type="hidden" name="method" value="updateProvince">
 		<div class="apiMethod">
-			<div class="h3 apiMethodTitle">findVersions</div>
+			<div class="h3 apiMethodTitle">updateProvince</div>
 			<div class="apiParameter">
-				<span class="label apiLabel">product</span>
-				<input type="text" name="product" class="value input apiInput"/>
+				<span class="label apiLabel">name</span>
+				<input type="text" name="name" class="value input apiInput"/>
 			</div>
 			<div class="apiParameter">
-				<span class="label apiLabel">status</span>
-				<input type="text" name="status" class="value input apiInput"/>
+				<span class="label apiLabel">country_id</span>
+				<input type="text" name="country_id" class="value input apiInput"/>
+			</div>
+			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
+		</div>
+		</form>
+		<form method="post" action="<?=PATH?>/_geography/api" name="getProvince">
+		<input type="hidden" name="method" value="getProvince">
+		<div class="apiMethod">
+			<div class="h3 apiMethodTitle">getProvince</div>
+			<div class="apiParameter">
+				<span class="label apiLabel">country_id</span>
+				<input type="text" name="country_id" class="value input apiInput"/>
+			</div>
+			<div class="apiParameter">
+				<span class="label apiLabel">name</span>
+				<input type="text" name="name" class="value input apiInput"/>
+			</div>
+			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
+		</div>
+		</form>
+		<form method="post" action="<?=PATH?>/_geography/api" name="findProvinces">
+		<input type="hidden" name="method" value="findProvinces">
+		<div class="apiMethod">
+			<div class="h3 apiMethodTitle">findProvinces</div>
+			<div class="apiParameter">
+				<span class="label apiLabel">country_id</span>
+				<input type="text" name="country_id" class="value input apiInput"/>
 			</div>
 			<div class="apiMethodFooter"><input type="submit" name="btn_submit" value="Submit" class="button apiMethodSubmit"/></div>
 		</div>
