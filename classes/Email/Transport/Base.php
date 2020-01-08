31d5c6ec4be9929a31e4a94ba5ad5b835387d8d5
Anthony Caravello <tony@caravello.us>
2020-01-08 15:50:50 -0500

    Added some missing required methods to Email::Transport::Base

--- classes/Email/Transport/Base.php
+++ classes/Email/Transport/Base.php
@@ -4,6 +4,14 @@
 	class Base {
 		private $_error;
 
+		public function hostname() {
+			return $this->hostname;
+		}
+
+		public function token() {
+			return $this->token;
+		}
+
 		public function result() {
 			return $this->_result;
 		}
