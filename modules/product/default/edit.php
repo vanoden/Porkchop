994709c351fd9d03a4652bf3097510230cec3e46
Anthony Caravello <tony@caravello.us>
2020-07-20 11:09:52 -0400

    Updated Product Editing to fix metadata issues and handle updates to code using id as key

--- modules/product/default/edit.php
+++ modules/product/default/edit.php
@@ -15,25 +15,33 @@
 		document.getElementById('ItemImageDiv_'+code).style.display = "none";
 	}
 </script>
-<h2>Edit Product</h2>
+<h2><?php
+	if (isset($item->id)) print "Edit Product '".$item->code."'";
+	else print "Add product"; ?>
+</h2>
 <?php	if ($page->errorCount() > 0) { ?>
 <div class="form_error"><?=$page->errorString()?></div>
 <?php	} ?>
 <form name="productEdit" method="post" action="/_product/edit">
-<input type="hidden" name="code" id="code" value="<?=$item->code?>" />
+<input type="hidden" name="id" id="id" value="<?=$item->id?>" />
 <input type="hidden" name="deleteImage" id="deleteImage" value="" />
 <div class="body">
 	<div class="input-horiz" id="itemCode">
 		<span class="label">Code</span>
-		<span class="value"><?=$item->code?></span>
+		<input type="text" name="code" value="<?=$item->code?>" class="value input" />
 	</div>
 	<div class="input-horiz" id="itemType">
 		<span class="label">Type</span>
-		<span class="value"><?=$item->type?></span>
+		<select class="input value" name="type">
+			<option value="">Select</option>
+<?php	foreach ($item_types as $item_type) { ?>
+			<option value="<?=$item_type?>"<?php if ($item_type == $item->type) print " selected";?>><?=$item_type?></option>
+<?php	} ?>
+		</select>
 	</div>
 	<div class="input-horiz" id="itemName">
 		<span class="label">Name</span>
-		<input type="text" class="value input wide_lg" name="name" id="name" value="<?=$item->name?>" />
+		<input type="text" class="value input wide_lg" name="name" id="name" value="<?=$item->metadata['name']?>" />
 	</div>
 	<div class="input-horiz" id="itemStatus">
 		<span class="label">Status</span>
@@ -46,31 +54,31 @@
 	</div>
 	<div class="input-horiz" id="itemShortDescription">
 		<span class="label align-top">Short Description</span>
-		<textarea class="value input wide_lg" name="short_description" id="short_description"><?=$item->short_description?></textarea>
+		<textarea class="value input wide_lg" name="short_description" id="short_description"><?=$item->metadata['short_description']?></textarea>
 	</div>
 	<div class="input-horiz" id="itemDescription">
 		<span class="label align-top">Description</span>
-		<textarea class="value input wide_lg" name="description" id="description"><?=$item->description?></textarea>
+		<textarea class="value input wide_lg" name="description" id="description"><?=$item->metadata['description']?></textarea>
 	</div>
 	<div class="input-horiz">
 		<span class="label">Model</span>
-		<input type="text" class="value input wide_md" name="model" id="model" value="<?=$item->model?>" />
+		<input type="text" class="value input wide_md" name="model" id="model" value="<?=$item->metadata['model']?>" />
 	</div>
 	<div class="input-horiz">
 		<span class="label">Empirical Formula</span>
-		<input type="text" class="value input wide_md" name="empirical_formula" id="empirical_formula" value="<?=$item->empirical_formula?>" />
+		<input type="text" class="value input wide_md" name="empirical_formula" id="empirical_formula" value="<?=$item->metadata['empirical_formula']?>" />
 	</div>
 	<div class="input-horiz">
 		<span class="label">Sensitivity</span>
-		<input type="text" class="value input wide_md" name="sensitivity" id="sensitivity" value="<?=$item->sensitivity?>" />
+		<input type="text" class="value input wide_md" name="sensitivity" id="sensitivity" value="<?=$item->metadata['sensitivity']?>" />
 	</div>
 	<div class="input-horiz">
 		<span class="label">Measure Range</span>
-		<input type="text" class="value input wide_md" name="measure_range" id="measure_range" value="<?=$item->measure_range?>" />
+		<input type="text" class="value input wide_md" name="measure_range" id="measure_range" value="<?=$item->metadata['measure_range']?>" />
 	</div>
 	<div class="input-horiz">
 		<span class="label">Accuracy</span>
-		<input type="text" class="value input wide_md" name="accuracy" id="accuracy" value="<?=$item->accuracy?>" />
+		<input type="text" class="value input wide_md" name="accuracy" id="accuracy" value="<?=$item->metadata['accuracy']?>" />
 	</div>
 	<div class="input-horiz">
 		<span class="label">Default Dashboard</span>
--- modules/product/default/edit_mc.php
+++ modules/product/default/edit_mc.php
@@ -2,25 +2,66 @@
 	$page = new \Site\Page();
 	$page->requireRole('product manager');
 
-	# Initialize Class
-	$item = new \Product\Item();
+	# Valid Item Types
+	$item_types = array(
+		"inventory",
+		"unique",
+		"group",
+		"kit",
+		"note"
+	);
 
+	$new_item = false;
 	# Fetch Code from Query String if not Posted
-	if (! $_REQUEST['code'])
-		$_REQUEST['code'] = $GLOBALS['_REQUEST_']->query_vars_array[0];
+	if (isset($_REQUEST['id']) && $_REQUEST['id'] > 0) {
+		$item = new \Product\Item($_REQUEST['id']);
+	}
+	elseif (isset($_REQUEST['code']) && $_REQUEST['code']) {
+		$item = new \Product\Item();
+		$item->get($_REQUEST['code']);
+		if ($item->id) $new_item = false;
+		else $new_item = true;
+	}
+	elseif (isset($GLOBALS['_REQUEST_']->query_vars_array[0])) {
+		$item = new \Product\Item();
+		$item->get($GLOBALS['_REQUEST_']->query_vars_array[0]);
+		if ($item->id) $new_item = false;
+		else $new_item = true;
+	}
+	else {
+		$new_item = true;
+	}
 
-	# Get Product
-	$item->get($_REQUEST['code']);
-
+	if (! $new_item && ! $item->id) {
+		$page->addError("Item not found");
+	}
 	# Handle Actions
-	if ($_REQUEST['submit'] == "Update") {
+	elseif ($_REQUEST['submit']) {
 		if (! $_REQUEST['code']) {
 			$page->addError("Code required");
 		}
+		elseif (! $_REQUEST['status']) {
+			$page->addError("Status required");
+		}
+		elseif (! $_REQUEST['type']) {
+			$page->addError("Type required");
+		}
 		else {
-			app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." editing product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
+			if ($new_item) {
+				app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." editing product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
+				$item->add(array(
+					"code"	=> $_REQUEST['code'],
+					"type"	=> $_REQUEST['type'],
+					"status"	=> $_REQUEST['status']
+				));
+			}
+			else
+				app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." adding product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
+
 			$item->update(
 				array(
+					"code"	=> $_REQUEST['code'],
+					"type"	=> $_REQUEST['type'],
 					"status"			=> $_REQUEST["status"]
 				)
 			);
@@ -28,6 +69,32 @@
 				app_log("Error updating item: ".$item->error,'error',__FILE__,__LINE__);
 				$page->addError("Error updating Item");
 			}
+
+			# Associate with a Parent if one selected or new product
+			if ($_REQUEST['parent_code']) {
+				$parent = new \Product\Item();
+				$parent->get($_REQUEST['parent_code']);
+				if ($parent->error) {
+					app_log("Error finding item ".$_REQUEST['parent_code'],'error',__FILE__,__LINE__);
+					$page->addError("Error finding parent");
+				}
+				elseif ($parent->id) {
+					$relationship = new \Product\Relationship();
+					$relationship->add(array(
+						"parent_id"	=> $parent->id,
+						"child_id" => $item->id
+					));
+				}
+			}
+			elseif ($new_item) {
+				$relationship = new \Product\Relationship();
+				$relationship->add(array(
+					"parent_id"	=> 0,
+					"child_id" => $item->id
+				));
+			}
+
+			# Add Company Specific Metadata (REPLACE WITH CONFIGURED LOOP OF KEYS)
 			$item->addMeta("name",$_REQUEST["name"]);
 			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
 			$item->addMeta("short_description",$_REQUEST["short_description"]);
@@ -61,91 +128,6 @@
 				$item->dropImage($image->id);
 			}
 		}
-	}
-	elseif ($_REQUEST['submit'] == "Add") {
-		if (! $_REQUEST['code']) {
-			$page->addError("Code required");
-		}
-		elseif (! $_REQUEST['status']) {
-			$page->addError("Status required");
-		}
-		elseif (! $_REQUEST['type']) {
-			$page->addError("Type required");
-		}
-		elseif ($item->id) {
-			$page->addError("Product with code already exists");
-		}
-		else {
-			app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." adding product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
-			$item->add(
-				array(
-					"code"		=> $_REQUEST["code"],
-					"status"	=> $_REQUEST["status"],
-					"type"		=> $_REQUEST["type"],
-				)
-			);
-			if ($item->error)
-			{
-				app_log("Error adding item: ".$item->error,'error',__FILE__,__LINE__);
-				$page->addError("Error adding Item");
-			}
-			else
-			{
-				$parent = new \Product\Item();
-				$parent->get($_REQUEST['parent_code']);
-				if ($parent->error) {
-					app_log("Error finding item ".$_REQUEST['parent_code'],'error',__FILE__,__LINE__);
-					$page->addError("Error finding parent");
-				}
-				elseif ($parent->id) {
-					$relationship = new \Product\Relationship();
-					$relationship->add(array(
-						"parent_id"	=> $parent->id,
-						"child_id" => $item->id
-					));
-				}
-				else
-				{
-					$relationship = new \Product\Relationship();
-					$relationship->add(array(
-						"parent_id"	=> 0,
-						"child_id" => $item->id
-					));
-				}
-				$item->addMeta("name",$_REQUEST["name"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("short_description",$_REQUEST["short_description"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("description",$_REQUEST["description"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("model",$_REQUEST["model"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("empirical_formula",$_REQUEST["empirical_formula"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("sensitivity",$_REQUEST["sensitivity"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("measure_range",$_REQUEST["measure_range"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("accuracy",$_REQUEST["accuracy"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("manual_id",$_REQUEST["manual_id"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("spec_table_image",$_REQUEST["spec_table_image"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-				$item->addMeta("default_dashboard_id",$_REQUEST["default_dashboard_id"]);
-				if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-
-				if ($_REQUEST['new_image_code'])
-				{
-					$image = new \Media\Image();
-					$image->get($_REQUEST['new_image_code']);
-					$item->addImage($image->id);
-				}
-			}
-		}
-	}
-	elseif($_REQUEST['submit']){
-		$page->addError("Invalid request");
 	}
 
 	# Get Product
