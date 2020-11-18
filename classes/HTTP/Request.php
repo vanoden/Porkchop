83066ff74139169879519185e0bc1cfef84b9f76
Anthony Caravello <tony@caravello.us>
2020-11-18 11:29:54 -0500

    replaced split with str_split for PHP7

--- classes/HTTP/Request.php
+++ classes/HTTP/Request.php
@@ -194,7 +194,7 @@
 				if (preg_match('/(.*)\?([\w\.\_\-]+\=.*)/',$element,$matches)) {
 					$element = $matches[1];
 					$rest = $matches[2];
-					$elements = split("&",$rest);
+					$elements = str_split("&",$rest);
 					foreach($elements as $element) {
 						if (preg_match("/=/",$element)) {
 							list($label,$value) = preg_split("/=/",$element);
