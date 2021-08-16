44aaba1810ac4353c19d23d75afb14e9eda806b1
Anthony Caravello <tony@caravello.us>
2021-08-16 15:35:42 -0400

    Moved spectros view to its own repo

--- modules/spectros/default/admin_home_mc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (role('register manager') || role('monitor admin') || role('action user')) {
-		# You are amongst friends
-	}
-	else {
-        print "<p><strong>This area is for administrators only!</string></p>";
-        print "<p>Return to <a href=\"/\">Spectros Instruments Web Site</a></p>";
-        exit;
-    }
--- modules/spectros/default/admin_requests_mc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_module("action");
-    
-    $_requests = new ActionRequests();
-    $requests = $_requests->find(
-        array(
-            "status"    => array("NEW","ASSIGNED","OPEN")
-        )
-    );
