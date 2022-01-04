9796479d271f6f180a8f12611d935617c905e267
Anthony Caravello <tony@caravello.us>
2022-01-04 16:31:40 -0500

    Allow for creation of new roles

--- modules/register/default/role_mc.php
+++ modules/register/default/role_mc.php
@@ -2,41 +2,60 @@
 	$page = new \Site\Page();
 	$page->requireRole('register manager');
 
-    if ($_REQUEST['id']) $role = new \Register\Role($_REQUEST['id']);
-    else {
-    	$role_name = isset($_REQUEST['name']) ? $_REQUEST['name']: '';
-	    if (empty($role_name)) $role_name = $GLOBALS['_REQUEST_']->query_vars_array[0];
-
+	# Identify Specified Role if possible
+    if ($_REQUEST['id']) {
+		$role = new \Register\Role($_REQUEST['id']);
+		if (! $role->id) $page->addError("Role &quot;".$_REQUEST['id']."&quot; not found");
+	}
+	if (! $role->id && isset($_REQUEST['name']) && strlen($_REQUEST['name']) > 0) {
     	$role = new \Register\Role();
-    	$role->get($role_name);
-    }
-    if ($role->id) {
-		if (isset($_REQUEST['btn_submit'])) {
-			if ($role->update(
-				array(
-					'description'	=> $_REQUEST['description']
-				)
-			)) {
-				$page->success = "Role Updated";
-			} else {
-				$page->addError("Role update failed: ".$role->error);
+    	$role->get($_REQUEST['name']);
+		if (! $role->id) {
+			if (isset($_REQUEST['btn_submit'])) {
+				$role->add(array(
+					"name" => $_REQUEST['name'],
+					"description" => $_REQUEST['description']
+				));
+			}
+			else {
+				$page->addError("Role &quot;".$_REQUEST['name']."&quot; not found");
 			}
 		}
-	} else {
-		$page->addError("Role not found");
+	}
+	if (! $role->id && isset($GLOBALS['_REQUEST_']->query_vars_array[0]) && strlen($GLOBALS['_REQUEST_']->query_vars_array[0])) {
+    	$role_name = $GLOBALS['_REQUEST_']->query_vars_array[0];
+		$role = new \Register\Role();
+		$role->get($role_name);
+		if (! $role->id) $page->addError("Role named &quot;".$role_name."&quot; not found");
+    }
+
+    if ($role->id && isset($_REQUEST['btn_submit'])) {
+		if ($role->update(
+			array(
+				'description'	=> $_REQUEST['description']
+			)
+		)) {
+			$page->success = "Role Updated";
+		} else {
+			$page->addError("Role update failed: ".$role->error);
+		}
 	}
 
-	$privilegeList = new \Register\PrivilegeList();
-    $privileges = $privilegeList->find();
-    foreach ($privileges as $privilege) {
-        if ($_REQUEST['privilege'][$privilege->id] == 1) {
-            if (! $role->has_privilege($privilege->id) && $role->addPrivilege($privilege->id)) {
-                $page->success .= "Added privilege '".$privilege->name."'";
-            }
-        }
-        else {
-            if ($role->has_privilege($privilege->id) && $role->dropPrivilege($privilege->id)) {
-                $page->success .= "Removed privilege '".$privilege->name."'";
-            }
-        }
-    }
+	if ($role->id) {
+		$privilegeList = new \Register\PrivilegeList();
+	    $privileges = $privilegeList->find();
+		if (isset($_REQUEST['btn_submit'])) {
+		    foreach ($privileges as $privilege) {
+		        if ($_REQUEST['privilege'][$privilege->id] == 1) {
+		            if (! $role->has_privilege($privilege->id) && $role->addPrivilege($privilege->id)) {
+		                $page->success .= "Added privilege '".$privilege->name."'";
+		            }
+		        }
+		        else {
+		            if ($role->has_privilege($privilege->id) && $role->dropPrivilege($privilege->id)) {
+		                $page->success .= "Removed privilege '".$privilege->name."'";
+		            }
+		        }
+			}
+	    }
+	}
