2ca6867eec7aa2cf785c6171db57e3796ad3ccaf
Anthony Caravello <tony@caravello.us>
2022-01-27 21:31:30 -0500

    Replaced CKEditor with TinyCE, no more popup window for editing

--- modules/content/default/edit.php
+++ modules/content/default/edit.php
@@ -1,3 +1,11 @@
+<script src="https://cdn.tiny.cloud/1/owxjg74mr7ujxhw9soo7iquo7iul2mclregqovcp7ophazmn/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+	tinymce.init({
+		selector: '#content',
+        plugins: 'code',
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | code'
+	});
+</script>
 <form method="post" action="/_content/edit">
 <input type="hidden" name="id" value="<?=$message->id?>"/>
 <div>
@@ -6,7 +14,7 @@
     <span>Target</span>
     <input type="text" name="target" id="target" value="<?=$message->target?>"/>
     <span>Content</span>
-    <textarea id="content" name="content"><?=$message->content?></textarea>
+	<textarea id="content" name="content"><?=$message->content?></textarea>
 	<input type="submit" name="Submit" value="Submit"/>
 </div>
 </form>
--- modules/content/default/edit_mc.php
+++ modules/content/default/edit_mc.php
@@ -9,3 +9,14 @@
 			$message->target = $GLOBALS['_REQUEST_']->index;
 		}
 	}
+
+	if (isset($_REQUEST['Submit'])) {
+		if ($message->id) {
+			app_log("Updating content_message '".$message->target."'",'info');
+			$message->update(array('name' => $_REQUEST['name'], 'content' => $_REQUEST['content']));
+		}
+		else {
+			app_log("Adding content_message '".$_REQUEST['target']."'",'info');
+			$message->add(array('target' => $_REQUEST['target'],'name' => $_REQUEST['name'], 'content' => $_REQUEST['content']));
+		}
+	}
