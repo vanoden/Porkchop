e33c6c1fa0656ed515ee6ba10563d4a04fc27c9d
Anthony Caravello <tony@caravello.us>
2022-03-07 22:30:32 -0500

    Took messages_mc.php out of debug configuration

--- modules/site/default/messages_mc.php
+++ modules/site/default/messages_mc.php
@@ -4,6 +4,5 @@
 
 	if (empty($GLOBALS['_SESSION_']->customer->organization->id)) $page->addError("Your registration has not been completed.  Please make sure you've validated your email and contact ".$GLOBALS['_config']->site->support_email.' for assistance.');
 	$siteMessages = new \Site\SiteMessagesList();
-	//$userMessages = $siteMessages->find(array('user_created'=>$GLOBALS['_SESSION_']->customer->id));
-	
-	$userMessages = $siteMessages->find(array('user_created'=>2612));
+
+	$userMessages = $siteMessages->find(array('user_created'=>$GLOBALS['_SESSION_']->customer->id));
