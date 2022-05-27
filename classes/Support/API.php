e740b4ae9c0c285ef19e39ced26048a8422cb81b
Anthony Caravello <tony@caravello.us>
2022-05-27 12:35:58 -0400

    Replacing has_role() calls with more static can() calls in Support module and strengthened permissions in API

--- classes/Support/API.php
+++ classes/Support/API.php
@@ -17,26 +17,31 @@
 		 * Add a Request
 		 */
 		public function addRequest() {
+			$this->requireAuth();
 			$request = new \Support\Request ();
 			$parameters = array ();
-			if ($GLOBALS['_SESSION_']->customer->has_role( 'support manager' )) {
-				if ($_REQUEST ['customer']) {
-					$customer = new \Register\Customer ();
-					$customer->get ( $_REQUEST ['customer'] );
-					if ($customer->error) $this->app_error ( "Error getting customer: " . $customer->error, 'error', __FILE__, __LINE__ );
-					if (! $customer->id) $this->error ( "Customer not found" );
-					$parameters ['customer_id'] = $customer->id;
-				}
-				if ($_REQUEST ['tech']) {
-					$admin = new \Register\Admin ();
-					$admin->get ( $_REQUEST ['admin'] );
-					if ($admin->error) $this->app_error ( "Error getting admin: " . $admin->error, 'error', __FILE__, __LINE__ );
-					if (! $admin->id) $this->error ( "Tech not found" );
-					$parameters ['tech_id'] = $admin->id;
-				}
-				if ($_REQUEST ['status']) {
-					$parameters ['status'] = $_REQUEST ['status'];
-				}
+			if ($_REQUEST ['customer']) {
+				$customer = new \Register\Customer ();
+				$customer->get ( $_REQUEST ['customer'] );
+				if ($customer->error) $this->app_error ( "Error getting customer: " . $customer->error, 'error', __FILE__, __LINE__ );
+
+				if (!$GLOBALS['_SESSION_']->isCustomer($customer->id) && ! $GLOBALS['_SESSION_']->customer->can('manage support requests')) $this->deny();
+
+				if (! $customer->id) $this->error ( "Customer not found" );
+				$parameters ['customer_id'] = $customer->id;
+			}
+			if ($_REQUEST ['tech']) {
+				if (! $GLOBALS['_SESSION_']->customer->can("manage support requests")) $this->deny();
+
+				$admin = new \Register\Admin ();
+				$admin->get ( $_REQUEST ['admin'] );
+				if ($admin->error) $this->app_error ( "Error getting admin: " . $admin->error, 'error', __FILE__, __LINE__ );
+				if (! $admin->id) $this->error ( "Tech not found" );
+				$parameters ['tech_id'] = $admin->id;
+			}
+			if ($_REQUEST ['status']) {
+				if (! $_REQUEST['status'] == "NEW" && ! $GLOBALS['_SESSION_']->customer->can('manage support requests')) $this->deny();
+				$parameters ['status'] = $_REQUEST ['status'];
 			}
 
 			$request->add ( $parameters );
@@ -52,10 +57,13 @@
 		 * Update a Request
 		 */
 		public function updateRequest() {
+			$this->requireAuth();
 			$request = new \Support\Request ();
 			$request->get ( $_REQUEST ['code'] );
 			if ($request->error) $this->app_error ( "Error finding request: " . $request->error, 'error', __FILE__, __LINE__ );
 			if (! $request->id) $this->error ( "Request not found" );
+
+			if (!$GLOBALS['_SESSION']->isOrganization($request->customer->organization_id) && !$GLOBALS['_SESSION_']->customer->can('manage support requests')) $this->deny();
 
 			$request->update ( array ('name' => $_REQUEST ['name'],'type' => $_REQUEST ['type'],'status' => $_REQUEST ['status'],'description' => $_REQUEST ['description'] ) );
 			if ($request->error) $this->app_error ( "Error adding product: " . $request->error, 'error', __FILE__, __LINE__ );
@@ -70,10 +78,13 @@
 		 * Get Specified Request
 		 */
 		public function getRequest() {
+			$this->requireAuth();
 			$request = new \Support\Request ();
 			$request->get ( $_REQUEST ['code'] );
 
 			if ($request->error) $this->error ( "Error getting request: " . $request->error );
+
+			if (!$GLOBALS['_SESSION']->isOrganization($request->customer->organization_id) && !$GLOBALS['_SESSION_']->customer->can('manage support requests')) $this->deny();
 
 			$this->response->success = 1;
 			$this->response->request = $request;
@@ -85,10 +96,13 @@
 		 * Find matching Requests
 		 */
 		public function findRequests() {
+			$this->requireAuth();
 			$requestlist = new \Support\RequestList ();
 
 			$parameters = array ();
 			if ($_REQUEST ['status']) $parameters ['status'] = $_REQUEST ['status'];
+
+			if (!$GLOBALS['_SESSION_']->customer->can('manage support requests')) $parameters['organization_id'] = $GLOBALS['_SESSION_']->customer->organization->id;
 
 			$requests = $requestlist->find ( $parameters );
 			if ($requestlist->error) $this->app_error ( "Error finding requests: " . $requestlist->error );
