cf36cdfa98b660bb1c1b96db420cd01c6478de51
Anthony Caravello <tony@caravello.us>
2022-05-27 14:20:27 -0400

    admin based on 'see admin tools' privilege

--- classes/Register/API.php
+++ classes/Register/API.php
@@ -19,8 +19,8 @@
         function me() {
             # Default StyleSheet
             if (! isset($_REQUEST["stylesheet"])) $_REQUEST["stylesheet"] = 'register.customer.xsl';
-            if ($GLOBALS['_SESSION_']->customer->has_role('administrator')) $GLOBALS['_SESSION_']->customer->admin = 1;
-            
+            if ($GLOBALS['_SESSION_']->customer->can('see admin tools')) $GLOBALS['_SESSION_']->customer->admin = 1;
+ 
             $siteMessageDeliveryList = new \Site\SiteMessageDeliveryList();
             $siteMessageDeliveryList->find(array('user_id' => $GLOBALS['_SESSION_']->customer->id, 'acknowledged' => false));
             $siteMessagesUnread = $siteMessageDeliveryList->count();
