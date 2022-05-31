af9271cb14f59934e5b9f9d9bd1725ec5088e31c
Anthony Caravello <tony@caravello.us>
2022-05-30 23:14:50 -0400

    Fixed cache setting

--- classes/Cache/Client/Memcache.php
+++ classes/Cache/Client/Memcache.php
@@ -40,7 +40,8 @@
 			return false;
 		}
 
-		public function set($key,$value,$expires=$GLOBALS['_cache']->default_expire_seconds) {
+		public function set($key,$value,$expires = null) {
+			if (!isset($expires)) $expires = $GLOBALS['_config']->cache->default_expire_seconds;
 			if ($this->_connected) {
 				if ($this->_service->set($key,$value,$expires)) return true;
 				else $this->error = "Error storing cache value for '$key': ".$this->_service->getResultCode();
