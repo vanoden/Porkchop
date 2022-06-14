615deb9d94fdd5ae2366e54b50accf883c0a35a5
Anthony Caravello <tony@caravello.us>
2022-06-14 13:35:47 -0400

    Extra ReCAPTCHA to its own class

---
+++ classes/GoogleAPI/ReCAPTCHA.php
@@ -0,0 +1,39 @@
+<?php
+	namespace GoogleAPI;
+
+	class ReCAPTCHA Extends \BaseClass {	
+		public function test($customer,$response) {
+			# Check reCAPTCHA
+			$url = "https://www.google.com/recaptcha/api/siteverify";
+			$data = array(
+				'secret'	=> $GLOBALS['_config']->captcha->private_key,
+				'response'	=> $response,
+				'remoteip'	=> $_SERVER['REMOTE_ADDR'],
+			);
+
+			$options = array(
+				'http'	=> array(
+					'method'	=> 'POST',
+					'content'	=> http_build_query($data),
+				),
+			);
+							
+			# Don't need to store these fields
+			unset($_REQUEST['g-recaptcha-response']);
+			unset($_REQUEST['btn_submit']);
+
+			$context = stream_context_create($options);
+			$result = file_get_contents($url,false,$context);
+			$captcha_success = json_decode($result);
+			
+			if ($captcha_success->success == true ) {
+				app_log("ReCAPTCHA presented and SOLVED for " . $customer->status . " Customer (must be a human attempting)" , 'notice' , __FILE__ , __LINE__);
+				$customer->update(array('status' => 'ACTIVE'));
+				return true;
+			} else {
+				$this->error("Sorry, CAPTCHA Invalid.  Please Try Again");
+				app_log("ReCAPTCHA presented and FAILED for " . $customer->status . " Customer" , 'notice' , __FILE__ , __LINE__);
+				return false;
+			}
+		}
+	}
