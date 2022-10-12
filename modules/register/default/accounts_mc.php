ea0d4c9447db12ff998efe1b3e82e22a13ad6392
Anthony Caravello <tony@caravello.us>
2022-10-12 15:54:37 -0400

    Sanitize input in register/accounts

--- modules/register/default/accounts_mc.php
+++ modules/register/default/accounts_mc.php
@@ -5,7 +5,7 @@
 	### for the user.								###
 	### A. Caravello 11/12/2002						###
 	###################################################
-	$page = new \Site\Page('register','accounts');
+	$page = new \Site\Page();
 	$page->requirePrivilege("manage customers");
 
 	// Initialize Parameter Array
@@ -28,12 +28,17 @@
 		return 403;
 
 	$customer_list = new \Register\CustomerList();
-		
+
+	if (!empty($_REQUEST['search']) && !preg_match('/^\*?[\w\-\.\_\s]+\*?$/',$_REQUEST['search'])) {
+		$page->addError("Invalid search string");
+		$_REQUEST['search'] = null;
+	}
+
 	$find_parameters['status'] = array('NEW','ACTIVE');
-	if (isset($_REQUEST['deleted'])) array_push($find_parameters['status'],'DELETED');
-	if (isset($_REQUEST['expired'])) array_push($find_parameters['status'],'EXPIRED');
-	if (isset($_REQUEST['hidden'])) array_push($find_parameters['status'],'HIDDEN');
-	if (isset($_REQUEST['blocked'])) array_push($find_parameters['status'],'BLOCKED');
+	if (isset($_REQUEST['deleted']) && $_REQUEST['deleted'] == 1) array_push($find_parameters['status'],'DELETED');
+	if (isset($_REQUEST['expired']) && $expired['deleted'] == 1) array_push($find_parameters['status'],'EXPIRED');
+	if (isset($_REQUEST['hidden']) && $_REQUEST['hidden'] == 1) array_push($find_parameters['status'],'HIDDEN');
+	if (isset($_REQUEST['blocked']) && $_REQUEST['blocked'] == 1) array_push($find_parameters['status'],'BLOCKED');
 	if (isset($_REQUEST['search']) && strlen($_REQUEST['search'])) $find_parameters['_search'] = $_REQUEST['search'];
 
 	// Get Count before Pagination
@@ -45,7 +50,7 @@
 	$find_parameters["_offset"] = isset($_REQUEST['start']) ? $_REQUEST['start'] : 0;
 	$customers = $customer_list->find($find_parameters);
 	if ($customer_list->error) $page->error = "Error finding customers: ".$customer_list->error;
-		
+
 	if (isset($_REQUEST['start']) && $_REQUEST['start'] < $customers_per_page)
 		$prev_offset = 0;
 	else
