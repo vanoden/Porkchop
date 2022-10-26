87d5e0a43f42ca77597951cc40db8aaf1a77bf86
Kevin Hinds <khinds10.work@gmail.com>
2022-10-26 14:45:48 -0400

    Web Portal Security Remediation             Product	add             Product	browse             Product	edit

--- modules/product/default/edit.php
+++ modules/product/default/edit.php
@@ -16,108 +16,109 @@
 	if (isset($item->id)) print "Edit Product '".$item->code."'";
 	else print "Add product"; ?>
 </h2>
-<?php	if ($page->errorCount() > 0) { ?>
-<div class="form_error"><?=$page->errorString()?></div>
-<?php	} ?>
+<?php if ($page->errorCount() > 0) { ?>
+    <div class="form_error"><?=$page->errorString()?></div>
+<?php } ?>
 <form name="productEdit" method="post" action="/_product/edit">
-<input type="hidden" name="id" id="id" value="<?=$item->id?>" />
-<input type="hidden" name="deleteImage" id="deleteImage" value="" />
-<div class="body">
-	<div class="input-horiz" id="itemCode">
-		<span class="label">Code</span>
-		<input type="text" name="code" value="<?=htmlspecialchars($item->code)?>" class="value input" />
-	</div>
-	<div class="input-horiz" id="itemType">
-		<span class="label">Type</span>
-		<select class="input value" name="type">
-			<option value="">Select</option>
-<?php	foreach ($item_types as $item_type) { ?>
-			<option value="<?=$item_type?>"<?php if ($item_type == $item->type) print " selected";?>><?=$item_type?></option>
-<?php	} ?>
-		</select>
-	</div>
-	<div class="input-horiz" id="itemName">
-		<span class="label">Name</span>
-		<input type="text" class="value input wide_lg" name="name" id="name" value="<?=htmlspecialchars($item->metadata['name'])?>" />
-	</div>
-	<div class="input-horiz" id="itemStatus">
-		<span class="label">Status</span>
-		<select name="status" class="value input wide_sm">
-			<option value="">Select</option>
-			<option value="ACTIVE"<?php if ($item->status == 'ACTIVE') print " selected"; ?>>ACTIVE</option>
-			<option value="HIDDEN"<?php if ($item->status == 'HIDDEN') print " selected"; ?>>HIDDEN</option>
-			<option value="DELETED"<?php if ($item->status == 'DELETED') print " selected"; ?>>DELETED</option>
-		</select>
-	</div>
-	<div class="input-horiz" id="itemShortDescription">
-		<span class="label align-top">Short Description</span>
-		<textarea class="value input wide_lg" name="short_description" id="short_description"><?=htmlspecialchars($item->metadata['short_description'])?></textarea>
-	</div>
-	<div class="input-horiz" id="itemDescription">
-		<span class="label align-top">Description</span>
-		<textarea class="value input wide_lg" name="description" id="description"><?=htmlspecialchars($item->metadata['description'])?></textarea>
-	</div>
-	<div class="input-horiz">
-		<span class="label">Model</span>
-		<input type="text" class="value input wide_md" name="model" id="model" value="<?=htmlspecialchars($item->metadata['model'])?>" />
-	</div>
-	<div class="input-horiz">
-		<span class="label">Empirical Formula</span>
-		<input type="text" class="value input wide_md" name="empirical_formula" id="empirical_formula" value="<?=htmlspecialchars($item->metadata['empirical_formula'])?>" />
-	</div>
-	<div class="input-horiz">
-		<span class="label">Sensitivity</span>
-		<input type="text" class="value input wide_md" name="sensitivity" id="sensitivity" value="<?=htmlspecialchars($item->metadata['sensitivity'])?>" />
-	</div>
-	<div class="input-horiz">
-		<span class="label">Measure Range</span>
-		<input type="text" class="value input wide_md" name="measure_range" id="measure_range" value="<?=htmlspecialchars($item->metadata['measure_range'])?>" />
-	</div>
-	<div class="input-horiz">
-		<span class="label">Accuracy</span>
-		<input type="text" class="value input wide_md" name="accuracy" id="accuracy" value="<?=htmlspecialchars($item->metadata['accuracy'])?>" />
-	</div>
-	<div class="input-horiz">
-		<span class="label">Default Dashboard</span>
-		<select class="value input wide_md" name="default_dashboard_id" id="default_dashboard_id">
-<?php	$default_dashboard = $item->getMetadata('default_dashboard_id');
-	foreach($dashboards as $dashboard) { ?>
-			<option value="<?=$dashboard->id?>"<?php if ($default_dashboard->value == $dashboard->id) { print " selected"; } ?>><?=$dashboard->name?></option>
-<?php	} ?>
-		</select>
-	</div>
-	<div class="input-horiz">
-		<span class="label">Manual</span>
-		<select class="value input wide_md" name="manual_id" id="manual_id">
-<?php	foreach($manuals as $manual) { ?>
-			<option value="<?=$manual->id?>"<?php if ($item->manual_id == $manual->id) { print " selected"; } ?>><?=$manual->name?></option>
-<?php	} ?>
-		</select>
-	</div>
-	<div class="input-horiz">
-		<span class="label">Spec Table</span>
-		<select class="value input wide_md" name="spec_table_image" id="spec_table_image">
-			<option value="Select"></option>
-<?php	foreach($tables as $table) { ?>
-			<option value="<?=$table->id?>"<?php if ($item->spec_table_image == $table->id) { print " selected"; } ?>><?=$table->name?></option>
-<?php	} ?>
-		</select>
-	</div>
-	<div class="input-horiz" id="itemImages">
-		<span class="label align-top">Images</span>
-<?php	foreach($item->image as $image) { ?>
-		<div class="editItemImage" id="ItemImageDiv_<?=$image->code?>">
-			<input type="button" name="btn_drop" class="editItemThumbnail" onclick="dropImage('<?=$image->code?>')" value="X" />
-			<img class="editItemThumbnail" src="/_media/api?method=downloadMediaFile&code=<?=$image->files[0]->code?>">
-		</div>
-<?php	} ?>
-		<div class="editItemImage" id="newImageBox">
-            <input type="button" name="addImageButton" value="" class="add-image" onclick="initImageSelectWizard()" />
-            <input type="hidden" name="new_image_code" id="new_image_code" />
-        </div>
-	</div>
-	<div class="editSubmit button-bar floating">
-		<input type="submit" class="button" value="Update" name="submit" id="submit"/>
-	</div>
-</div>
+    <input type="hidden" name="id" id="id" value="<?=$item->id?>" />
+    <input type="hidden" name="deleteImage" id="deleteImage" value="" />
+    <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
+    <div class="body">
+	    <div class="input-horiz" id="itemCode">
+		    <span class="label">Code</span>
+		    <input type="text" name="code" value="<?=htmlspecialchars($item->code)?>" class="value input" />
+	    </div>
+	    <div class="input-horiz" id="itemType">
+		    <span class="label">Type</span>
+		    <select class="input value" name="type">
+			    <option value="">Select</option>
+                <?php foreach ($item_types as $item_type) { ?>
+			        <option value="<?=$item_type?>"<?php if ($item_type == $item->type) print " selected";?>><?=$item_type?></option>
+                <?php } ?>
+		    </select>
+	    </div>
+	    <div class="input-horiz" id="itemName">
+		    <span class="label">Name</span>
+		    <input type="text" class="value input wide_lg" name="name" id="name" value="<?=htmlspecialchars($item->metadata['name'])?>" />
+	    </div>
+	    <div class="input-horiz" id="itemStatus">
+		    <span class="label">Status</span>
+		    <select name="status" class="value input wide_sm">
+			    <option value="">Select</option>
+			    <option value="ACTIVE"<?php if ($item->status == 'ACTIVE') print " selected"; ?>>ACTIVE</option>
+			    <option value="HIDDEN"<?php if ($item->status == 'HIDDEN') print " selected"; ?>>HIDDEN</option>
+			    <option value="DELETED"<?php if ($item->status == 'DELETED') print " selected"; ?>>DELETED</option>
+		    </select>
+	    </div>
+	    <div class="input-horiz" id="itemShortDescription">
+		    <span class="label align-top">Short Description</span>
+		    <textarea class="value input wide_lg" name="short_description" id="short_description"><?=htmlspecialchars($item->metadata['short_description'])?></textarea>
+	    </div>
+	    <div class="input-horiz" id="itemDescription">
+		    <span class="label align-top">Description</span>
+		    <textarea class="value input wide_lg" name="description" id="description"><?=htmlspecialchars($item->metadata['description'])?></textarea>
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Model</span>
+		    <input type="text" class="value input wide_md" name="model" id="model" value="<?=htmlspecialchars($item->metadata['model'])?>" />
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Empirical Formula</span>
+		    <input type="text" class="value input wide_md" name="empirical_formula" id="empirical_formula" value="<?=htmlspecialchars($item->metadata['empirical_formula'])?>" />
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Sensitivity</span>
+		    <input type="text" class="value input wide_md" name="sensitivity" id="sensitivity" value="<?=htmlspecialchars($item->metadata['sensitivity'])?>" />
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Measure Range</span>
+		    <input type="text" class="value input wide_md" name="measure_range" id="measure_range" value="<?=htmlspecialchars($item->metadata['measure_range'])?>" />
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Accuracy</span>
+		    <input type="text" class="value input wide_md" name="accuracy" id="accuracy" value="<?=htmlspecialchars($item->metadata['accuracy'])?>" />
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Default Dashboard</span>
+		    <select class="value input wide_md" name="default_dashboard_id" id="default_dashboard_id">
+            <?php $default_dashboard = $item->getMetadata('default_dashboard_id');
+            	foreach($dashboards as $dashboard) { ?>
+		        	<option value="<?=$dashboard->id?>"<?php if ($default_dashboard->value == $dashboard->id) { print " selected"; } ?>><?=$dashboard->name?></option>
+            <?php } ?>
+		    </select>
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Manual</span>
+		    <select class="value input wide_md" name="manual_id" id="manual_id">
+                <?php foreach($manuals as $manual) { ?>
+			        <option value="<?=$manual->id?>"<?php if ($item->manual_id == $manual->id) { print " selected"; } ?>><?=$manual->name?></option>
+                <?php } ?>
+		    </select>
+	    </div>
+	    <div class="input-horiz">
+		    <span class="label">Spec Table</span>
+		    <select class="value input wide_md" name="spec_table_image" id="spec_table_image">
+			    <option value="Select"></option>
+                <?php foreach($tables as $table) { ?>
+			        <option value="<?=$table->id?>"<?php if ($item->spec_table_image == $table->id) { print " selected"; } ?>><?=$table->name?></option>
+                <?php } ?>
+		    </select>
+	    </div>
+	    <div class="input-horiz" id="itemImages">
+		    <span class="label align-top">Images</span>
+            <?php foreach($item->image as $image) { ?>
+		        <div class="editItemImage" id="ItemImageDiv_<?=$image->code?>">
+			        <input type="button" name="btn_drop" class="editItemThumbnail" onclick="dropImage('<?=$image->code?>')" value="X" />
+			        <img class="editItemThumbnail" src="/_media/api?method=downloadMediaFile&code=<?=$image->files[0]->code?>">
+		        </div>
+            <?php } ?>
+		    <div class="editItemImage" id="newImageBox">
+                <input type="button" name="addImageButton" value="" class="add-image" onclick="initImageSelectWizard()" />
+                <input type="hidden" name="new_image_code" id="new_image_code" />
+            </div>
+	    </div>
+	    <div class="editSubmit button-bar floating">
+		    <input type="submit" class="button" value="Update" name="submit" id="submit"/>
+	    </div>
+    </div>
 </form>
--- modules/product/default/edit_mc.php
+++ modules/product/default/edit_mc.php
@@ -3,13 +3,7 @@
 	$page->requirePrivilege('manage products');
 
 	// Valid Item Types
-	$item_types = array(
-		"inventory",
-		"unique",
-		"group",
-		"kit",
-		"note"
-	);
+	$item_types = array( "inventory", "unique", "group", "kit", "note" );
 
 	// Fetch Code from Query String if not Posted
 	$new_item = false;
@@ -30,96 +24,98 @@
 	}
 
 	if (! $new_item && ! $item->id) $page->addError("Item not found");
-	
+    	
 	// Handle Actions
 	elseif (isset($_REQUEST['submit'])) {
-		if (! isset($_REQUEST['code'])) {
-			$page->addError("Code required");
-		} elseif (! isset($_REQUEST['status'])) {
-			$page->addError("Status required");
-		} elseif (! isset($_REQUEST['type'])) {
-			$page->addError("Type required");
-		} else {
-			if ($new_item) {
-				app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." editing product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
-				$item->add(array(
-					"code"	=> $_REQUEST['code'],
-					"type"	=> $_REQUEST['type'],
-					"status"	=> $_REQUEST['status']
-				));
-			}
-			else
-				app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." adding product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
 
-			$item->update(
-				array(
-					"code"	=> $_REQUEST['code'],
-					"type"	=> $_REQUEST['type'],
-					"status"			=> $_REQUEST["status"]
-				)
-			);
-			if ($item->error) {
-				app_log("Error updating item: ".$item->error,'error',__FILE__,__LINE__);
-				$page->addError("Error updating Item");
-			}
+	    // CSRF Token Check
+	    if (! $GLOBALS['_SESSION_']->verifyCSRFToken($_POST['csrfToken'])) $page->addError("Invalid Request");
+	    if (!$page->errorCount()) {
 
-			# Associate with a Parent if one selected or new product
-			if ($_REQUEST['parent_code']) {
-				$parent = new \Product\Item();
-				$parent->get($_REQUEST['parent_code']);
-				if ($parent->error) {
-					app_log("Error finding item ".$_REQUEST['parent_code'],'error',__FILE__,__LINE__);
-					$page->addError("Error finding parent");
-				}
-				elseif ($parent->id) {
-					$relationship = new \Product\Relationship();
-					$relationship->add(array(
-						"parent_id"	=> $parent->id,
-						"child_id" => $item->id
-					));
-				}
-			}
-			elseif ($new_item) {
-				$relationship = new \Product\Relationship();
-				$relationship->add(array(
-					"parent_id"	=> 0,
-					"child_id" => $item->id
-				));
-			}
+		    if (! isset($_REQUEST['code'])) {
+			    $page->addError("Code required");
+		    } elseif (! isset($_REQUEST['status'])) {
+			    $page->addError("Status required");
+		    } elseif (! isset($_REQUEST['type'])) {
+			    $page->addError("Type required");
+		    } else {
+			    if ($new_item) {
+				    app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." editing product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
+				    $item->add(array(
+					    "code"	=> $_REQUEST['code'],
+					    "type"	=> $_REQUEST['type'],
+					    "status"	=> $_REQUEST['status']
+				    ));
+			    } else app_log("Admin ".$GLOBALS['_SESSION_']->customer->first_name." adding product ".$_REQUEST['code'],'notice',__FILE__,__LINE__);
 
-			# Add Company Specific Metadata (REPLACE WITH CONFIGURED LOOP OF KEYS)
-			$item->addMeta("name",noXSS($_REQUEST["name"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("short_description",noXSS($_REQUEST["short_description"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("description",noXSS($_REQUEST["description"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("model",noXSS($_REQUEST["model"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("empirical_formula",noXSS($_REQUEST["empirical_formula"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("sensitivity",noXSS($_REQUEST["sensitivity"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("measure_range",noXSS($_REQUEST["measure_range"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("accuracy",noXSS($_REQUEST["accuracy"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("manual_id",noXSS($_REQUEST["manual_id"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("spec_table_image",noXSS($_REQUEST["spec_table_image"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
-			$item->addMeta("default_dashboard_id",noXSS($_REQUEST["default_dashboard_id"]));
-			if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->update(
+				    array(
+					    "code"	=> $_REQUEST['code'],
+					    "type"	=> $_REQUEST['type'],
+					    "status"			=> $_REQUEST["status"]
+				    )
+			    );
+			    if ($item->error) {
+				    app_log("Error updating item: ".$item->error,'error',__FILE__,__LINE__);
+				    $page->addError("Error updating Item");
+			    }
 
-			$image = new \Media\Image();
-			if ($_REQUEST['new_image_code']) {
-				$image->get($_REQUEST['new_image_code']);
-				$item->addImage($image->id);
-			}
-			if ($_REQUEST['deleteImage']) {
-				$image->get($_REQUEST['deleteImage']);
-				$item->dropImage($image->id);
-			}
+			    # Associate with a Parent if one selected or new product
+			    if ($_REQUEST['parent_code']) {
+				    $parent = new \Product\Item();
+				    $parent->get($_REQUEST['parent_code']);
+				    if ($parent->error) {
+					    app_log("Error finding item ".$_REQUEST['parent_code'],'error',__FILE__,__LINE__);
+					    $page->addError("Error finding parent");
+				    } elseif ($parent->id) {
+					    $relationship = new \Product\Relationship();
+					    $relationship->add(array(
+						    "parent_id"	=> $parent->id,
+						    "child_id" => $item->id
+					    ));
+				    }
+			    } elseif ($new_item) {
+				    $relationship = new \Product\Relationship();
+				    $relationship->add(array(
+					    "parent_id"	=> 0,
+					    "child_id" => $item->id
+				    ));
+			    }
+
+			    // Add Company Specific Metadata (REPLACE WITH CONFIGURED LOOP OF KEYS)
+			    $item->addMeta("name",noXSS($_REQUEST["name"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("short_description",noXSS($_REQUEST["short_description"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("description",noXSS($_REQUEST["description"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("model",noXSS($_REQUEST["model"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("empirical_formula",noXSS($_REQUEST["empirical_formula"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("sensitivity",noXSS($_REQUEST["sensitivity"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("measure_range",noXSS($_REQUEST["measure_range"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("accuracy",noXSS($_REQUEST["accuracy"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("manual_id",noXSS($_REQUEST["manual_id"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("spec_table_image",noXSS($_REQUEST["spec_table_image"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+			    $item->addMeta("default_dashboard_id",noXSS($_REQUEST["default_dashboard_id"]));
+			    if ($item->error) app_log("Error setting metadata: ".$item->error,'error',__FILE__,__LINE__);
+
+			    $image = new \Media\Image();
+			    if ($_REQUEST['new_image_code']) {
+				    $image->get($_REQUEST['new_image_code']);
+				    $item->addImage($image->id);
+			    }
+			    if ($_REQUEST['deleteImage']) {
+				    $image->get($_REQUEST['deleteImage']);
+				    $item->dropImage($image->id);
+			    }
+		    }
 		}
 	}
 
