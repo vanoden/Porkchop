bad3a01f2c4e8f610271d7e36eb8f91ad4ea86da
Anthony Caravello <tony@caravello.us>
2022-12-05 14:37:07 -0500

    Changed Site::CounterList to use client counters() method

--- classes/Site/CounterList.php
+++ classes/Site/CounterList.php
@@ -2,13 +2,14 @@
     namespace Site;
 	class CounterList Extends \BaseClass {
 		public function find($parameters = array('showCacheObjects' => true)) {
-			$keys = $GLOBALS['_CACHE_']->keys();
-			$filteredKeys = array();
-			if ($parameters['showCacheObjects'] == false) {
-			    foreach ($keys as $key) if (!preg_match('/\[[0-9]+\]/', $key)) $filteredKeys[] = $key;			        
-			} else {
-    			$filteredKeys = $keys;
+			$this->clearErrors();
+			$this->resetCount();
+			$names = $GLOBALS['_CACHE_']->counters();
+			$counters = array();
+			foreach ($names as $name) {
+				$counter = new \Site\Counter("counter.".$name);
+				array_push($counters,$counter);
+				$this->incrementCount();
 			}
-			return $filteredKeys;	
 		}
 	}
