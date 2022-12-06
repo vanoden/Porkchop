74e9f080647718b320b9ccfabd2df214dcb88127
Anthony Caravello <tony@caravello.us>
2022-12-05 20:27:07 -0500

    Configurable max deletes and age threshold for cleaning session cache

--- modules/site/default/clean_cache_mc.php
+++ modules/site/default/clean_cache_mc.php
@@ -5,6 +5,13 @@
 	$client = $GLOBALS['_CACHE_'];
 
 	$max_deletes = 1000;
+	if (is_numeric($_REQUEST['max'])) $max_deletes = $_REQUEST['max_deletes'];
+
+	if (is_numeric($_REQUEST['age_days'])) $_REQUEST['age_hours'] = $_REQUEST['age_days'] * 24;
+	if (is_numeric($_REQUEST['age_hours'])) $hours = $_REQUEST['age_hours'];
+	else $horus = 24*7;
+	$age_threshold = time() - 3600 * $hours;
+
 	$deleted = 0;
 
 	$session_keys = $client->keys('session');
@@ -15,7 +22,7 @@
 
 		if (preg_match('/(\d+)\-(\d+)\-(\d+)\s(\d+)\:(\d+)\:(\d+)/',$session->last_hit_date,$matches)) {
 			$cmp_date = strtotime($matches[0]);
-			if (time() - $cmp_date > 604800) {
+			if (time() - $cmp_date > $age_threshold) {
 				print "Deleting session: ".$session->id." from ".$session->last_hit_date."<br>\n";
 				$client->delete($key);
 				$max_deletes --;
