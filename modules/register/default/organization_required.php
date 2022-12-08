bf42a6ffea40033f74bb2b244ede568b9df834a3
Anthony Caravello <tony@caravello.us>
2022-12-07 19:00:30 -0500

    Added message page for people not associated with an organization

---
+++ modules/register/default/organization_required.php
@@ -0,0 +1,8 @@
+<section id="form-message">
+	<ul class="connectBorder errorText">
+		<li>Your account must be associated with an organization to use this page.
+			If you're seeing this, your account probably has not yet been approved by our staff.
+			Please make sure you've validated your email address.  If so, we will approve your account shortly.
+		</li>
+	</ul>
+</section>
