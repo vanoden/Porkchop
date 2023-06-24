6994dec8e66de1e0bd1df3692dadac0fe28fc050
Chris Caravello <ccaravello@spectrosinstruments.com>
2023-06-24 01:07:47 +0100

    update admin account

--- modules/register/default/admin_account.php
+++ modules/register/default/admin_account.php
@@ -74,16 +74,18 @@
 <!-- End Page Header -->
 
 <form id="admin-account-form" name="register" action="<?= PATH ?>/_register/admin_account" method="POST">
-   <input type="hidden" name="csrfToken" value="<?= $GLOBALS['_SESSION_']->getCSRFToken() ?>">
-   <input type="hidden" name="target" value="<?= $target ?>" />
-   <input type="hidden" name="customer_id" value="<?= $customer_id ?>" />
-   <input type="hidden" name="login" value="<?= $customer->login ?>" />
+  <input type="hidden" name="csrfToken" value="<?= $GLOBALS['_SESSION_']->getCSRFToken() ?>">
+  <input type="hidden" name="target" value="<?= $target ?>" />
+  <input type="hidden" name="customer_id" value="<?= $customer_id ?>" />
+  <input type="hidden" name="login" value="<?= $customer->login ?>" />
 
-   <section id="form-message">
-    <ul class="connectBorder infoText"><li>Make changes and click 'Apply' to complete.</li></ul>
-   </section>
-
-   <!--	 Login Details -->
+  <section id="form-message">
+  <ul class="connectBorder infoText"><li>Make changes and click 'Apply' to complete.</li></ul>
+  </section>
+   
+   <!-- ============================================== -->
+   <!-- CUSTOMER LOGIN -->
+   <!-- ============================================== -->
   <div id="contact-main-table" class="tableBody min-tablet" style="font-size: 1.11rem;">
     <div class="tableRow" style="background: var(--color-light-one);">
       <div class="tableCell" style="width: 50%;">Login: <span class="value"><?= $customer->login ?></span></div>
@@ -95,174 +97,173 @@
       </div>
     </div>
   </div>
-
-   <!--	Start LOGIN Specs -->
-   <div class="tableBody clean min-tablet">
-      <div class="tableRowHeader">
-         <div class="tableCell" style="width: 30%;">Organization</div>
-         <div class="tableCell" style="width: 20%;">First Name</div>
-         <div class="tableCell" style="width: 20%;">Last Name</div>
-         <div class="tableCell" style="width: 30%;">Time Zone</div>
+   
+   <!-- ============================================== -->
+   <!-- LOGIN SPECIFICS -->
+   <!-- ============================================== -->
+  <div class="tableBody clean min-tablet">
+    <div class="tableRowHeader">
+      <div class="tableCell" style="width: 30%;">Organization</div>
+      <div class="tableCell" style="width: 20%;">First Name</div>
+      <div class="tableCell" style="width: 20%;">Last Name</div>
+      <div class="tableCell" style="width: 30%;">Time Zone</div>
+    </div>
+    <div class="tableRow">
+      <div class="tableCell">
+        <select class="value input registerValue" name="organization_id">
+          <option value="">Select</option>
+          <?php foreach ($organizations as $organization) { ?>
+            <option value="<?= $organization->id ?>" <?php if ($organization->id == $customer->organization()->id) print " selected"; ?>><?= $organization->name ?></option>
+          <?php } ?>
+        </select>
       </div>
-      <!-- end row header -->
-      <div class="tableRow">
-         <div class="tableCell">
-            <select class="value input registerValue" name="organization_id">
-               <option value="">Select</option>
-               <?php foreach ($organizations as $organization) { ?>
-                  <option value="<?= $organization->id ?>" <?php if ($organization->id == $customer->organization()->id) print " selected"; ?>><?= $organization->name ?></option>
-               <?php } ?>
-            </select>
-         </div>
-         <div class="tableCell">
-            <input type="text" class="value input registerValue registerFirstNameValue" name="first_name" value="<?= htmlentities($customer->first_name) ?>" />
-         </div>
-         <div class="tableCell">
-            <input type="text" class="value registerValue registerLastNameValue" name="last_name" value="<?= htmlentities($customer->last_name) ?>" />
-         </div>
-         <div class="tableCell">
-            <select id="timezone" name="timezone" class="value input collectionField">
-               <?php foreach (timezone_identifiers_list() as $timezone) {
-                  if (isset($customer->timezone))
-                     $selected_timezone = $customer->timezone;
-                  else
-                     $selected_timezone = 'UTC';
-                  ?>
-                  <option value="<?= $timezone ?>" <?php if ($timezone == $selected_timezone) print " selected"; ?>><?= $timezone ?></option>
-               <?php } ?>
-            </select>
-         </div>
+      <div class="tableCell"><input type="text" class="value input registerValue registerFirstNameValue" name="first_name" value="<?= htmlentities($customer->first_name) ?>" /></div>
+      <div class="tableCell"><input type="text" class="value registerValue registerLastNameValue" name="last_name" value="<?= htmlentities($customer->last_name) ?>" /></div>
+      <div class="tableCell">
+        <select id="timezone" name="timezone" class="value input collectionField">
+          <?php foreach (timezone_identifiers_list() as $timezone) {
+            if (isset($customer->timezone))
+                $selected_timezone = $customer->timezone;
+            else
+                $selected_timezone = 'UTC';
+            ?>
+            <option value="<?= $timezone ?>" <?php if ($timezone == $selected_timezone) print " selected"; ?>><?= $timezone ?></option>
+          <?php } ?>
+        </select>
       </div>
-   </div>
-   <!--End LOGIN Specs -->
-   <!-- START Methods of Contact -->
+    </div>
+  </div>
+   
+   <!-- ============================================== -->
+   <!-- METHODS OF CONTACT -->
+   <!-- ============================================== -->
    <h3>Methods of Contact</h3>
    <div id="contact-main-table" class="tableBody min-tablet">
-      <div class="tableRowHeader">
-         <div class="tableCell" style="width: 20%;">Type</div>
-         <div class="tableCell" style="width: 25%;">Description</div>
-         <div class="tableCell" style="width: 30%;">Address/Number</div>
-         <div class="tableCell" style="width: 15%;">Notes</div>
-         <div class="tableCell" style="width: 5%;">Notify</div>
-         <div class="tableCell" style="width: 5%;">Drop</div>
+    <div class="tableRowHeader">
+        <div class="tableCell" style="width: 20%;">Type</div>
+        <div class="tableCell" style="width: 25%;">Description</div>
+        <div class="tableCell" style="width: 30%;">Address/Number</div>
+        <div class="tableCell" style="width: 15%;">Notes</div>
+        <div class="tableCell" style="width: 5%;">Notify</div>
+        <div class="tableCell" style="width: 5%;">Drop</div>
+    </div>
+    <?php foreach ($contacts as $contact) { ?>
+    <div class="tableRow">
+      <div class="tableCell">
+        <select class="value input" name="type[<?= $contact->id ?>]">
+          <?php foreach (array_keys($contact_types) as $contact_type) { ?>
+              <option value="<?= $contact_type ?>" <?php if ($contact_type == $contact->type) print " selected"; ?>><?= $contact_types[$contact_type] ?></option>
+          <?php } ?>
+        </select>
       </div>
-      <!-- end row header -->
-      <?php foreach ($contacts as $contact) { ?>
-         <div class="tableRow">
-            <div class="tableCell">
-               <select class="value input" name="type[<?= $contact->id ?>]">
-                  <?php foreach (array_keys($contact_types) as $contact_type) { ?>
-                     <option value="<?= $contact_type ?>" <?php if ($contact_type == $contact->type) print " selected"; ?>><?= $contact_types[$contact_type] ?></option>
-                  <?php } ?>
-               </select>
-            </div>
-            <div class="tableCell">
-               <input type="text" name="description[<?= $contact->id ?>]" class="value wide_100per" value="<?= htmlentities($contact->description) ?>" />
-            </div>
-            <div class="tableCell">
-               <input type="text" name="value[<?= $contact->id ?>]" class="value wide_100per" value="<?= htmlentities($contact->value) ?>" />
-            </div>
-            <div class="tableCell">
-               <input type="text" name="notes[<?= $contact->id ?>]" class="value wide_100per" value="<?= htmlentities($contact->notes) ?>" />
-            </div>
-            <div class="tableCell">
-               <input type="checkbox" name="notify[<?= $contact->id ?>]" value="1" <?php if ($contact->notify) print "checked"; ?> />
-            </div>
-            <div class="tableCell">
-               <input type="image" id="dropitem" alt="remove item" src="/img/icons/icon_tools_trash_active.svg" style="cursor: pointer; border: none; width: 35px" onclick="submitDelete(<?= $contact->id ?>)">
-            </div>
-         </div>
-      <?php } ?>
-      <div class="tableRow">
-         <div class="tableCell">
-            <strong>New Contact Entry:</strong>
-            <select id="new-contact-select" class="value input" name="type[0]" onchange="enableNewContact()">
-               <?php foreach (array_keys($contact_types) as $contact_type) { ?>
-                  <option value="<?= $contact_type ?>"><?= $contact_types[$contact_type] ?></option>
-               <?php } ?>
-               <option value="0" selected="selected">Select</option>
-            </select>
-         </div>
-         <div class="tableCell">
-            <br />
-            <input type="text" id="new-description" name="description[0]" class="value wide_100per" style="display:none;" />
-         </div>
-         <div class="tableCell">
-            <br />
-            <input type="text" id="new-value" name="value[0]" class="value wide_100per" style="display:none;" />
-         </div>
-         <div class="tableCell">
-            <br />
-            <input type="text" id="new-notes" name="notes[0]" class="value wide_100per" style="display:none;" />
-         </div>
-         <div class="tableCell">
-            <br />
-            <input type="checkbox" id="new-notify" name="notify[0]" value="1" style="display:none;" />
-         </div>
-         <div class="tableCell">
-         </div>
+      <div class="tableCell"><input type="text" name="description[<?= $contact->id ?>]" class="value wide_100per" value="<?= htmlentities($contact->description) ?>" /></div>
+      <div class="tableCell"><input type="text" name="value[<?= $contact->id ?>]" class="value wide_100per" value="<?= htmlentities($contact->value) ?>" /></div>
+      <div class="tableCell"><input type="text" name="notes[<?= $contact->id ?>]" class="value wide_100per" value="<?= htmlentities($contact->notes) ?>" /></div>
+      <div class="tableCell"><input type="checkbox" name="notify[<?= $contact->id ?>]" value="1" <?php if ($contact->notify) print "checked"; ?> />
       </div>
+      <div class="tableCell"><input type="image" id="dropitem" alt="remove item" src="/img/icons/icon_tools_trash_act.svg" style="cursor: pointer; border: none; width: 30px" onclick="submitDelete(<?= $contact->id ?>)"></div>
+    </div>
+    <?php } ?>
+    <div class="tableRow">
+      <div class="tableCell">
+        <strong>New Contact Entry:</strong>
+        <select id="new-contact-select" class="value input" name="type[0]" onchange="enableNewContact()">
+          <?php foreach (array_keys($contact_types) as $contact_type) { ?>
+            <option value="<?= $contact_type ?>"><?= $contact_types[$contact_type] ?></option>
+          <?php } ?>
+          <option value="0" selected="selected">Select</option>
+        </select>
+      </div>
+      <div class="tableCell"><input type="text" id="new-description" name="description[0]" class="value wide_100per" style="display:none;" /></div>
+      <div class="tableCell"><input type="text" id="new-value" name="value[0]" class="value wide_100per" style="display:none;" /></div>
+      <div class="tableCell"><input type="text" id="new-notes" name="notes[0]" class="value wide_100per" style="display:none;" /></div>
+      <div class="tableCell"><input type="checkbox" id="new-notify" name="notify[0]" value="1" style="display:none;" /></div>
+      <div class="tableCell"></div>
    </div>
    <div style="text-align: left;">
       <input type="button" id="disable-new-contact-button" class="deleteButton" value="Cancel" style="cursor: pointer; display:none;" onclick="disableNewContact()" />
    </div>
-   <!--	END Methods of Contact -->
-   <!--	START Change Password-->
+   
+   <!-- ============================================== -->
+   <!-- CHANGE PASSWORD -->
+   <!-- ============================================== -->
    <?php if ($customer->auth_method == 'local') { ?>
-      <h3 class="marginTop_20">Change Password</h3>
-      <section id="form-message">
-        <ul class="connectBorder infoText"><li>Leave both fields empty for your password to stay the same.</li></ul>
-      </section>
-      <div class="tableBody clean min-tablet">
-         <div class="tableRowHeader">
-            <div class="tableCell" style="width: 30%;">New Password</div>
-            <div class="tableCell" style="width: 30%;">Confirm New Password</div>
-         </div>
-         <!-- end row header -->
-         <div class="tableRow">
-            <div class="tableCell">
-               <input type="password" class="value wide_100per" name="password" />
-            </div>
-            <div class="tableCell">
-               <input type="password" class="value wide_100per" name="password_2" />
-            </div>
-            <div class="tableCell">
-            </div>
-         </div>
+    <h3 class="marginTop_20">Change Password</h3>
+    <section id="form-message">
+      <ul class="connectBorder infoText"><li>Leave both fields empty for your password to stay the same.</li></ul>
+    </section>
+    <div class="tableBody clean min-tablet">
+      <div class="tableRowHeader">
+        <div class="tableCell" style="width: 30%;">New Password</div>
+        <div class="tableCell" style="width: 30%;">Confirm New Password</div>
       </div>
+      <!-- end row header -->
+      <div class="tableRow">
+        <div class="tableCell"><input type="password" class="value wide_100per" name="password" /></div>
+        <div class="tableCell"><input type="password" class="value wide_100per" name="password_2" /></div>
+        <div class="tableCell"></div>
+      </div>
+    </div>
    <?php } ?>
 
-   <!--	END Change Password-->
+<!-- ============================================== -->
+<!-- STATUS -->
+<!-- ============================================== -->
    <h3>Status</h3>
    <select class="input" name="status">
       <?php foreach (array('NEW', 'ACTIVE', 'EXPIRED', 'HIDDEN', 'DELETED', 'BLOCKED') as $status) { ?>
          <option value="<?= $status ?>" <?php if ($status == $customer->status) print " selected"; ?>><?= $status ?></option>
       <?php } ?>
    </select>
-   <h3>Assigned Roles</h3>
-   <table cellpadding="0" cellspacing="0" class="body">
+
+<!-- ============================================== -->
+<!-- ASSIGNED ROLES OPT 1 -->
+<!-- ============================================== -->
+    <h3>Assigned Roles</h3>
+    <table cellpadding="0" cellspacing="0" class="body">
       <tr>
-         <th class="label" style="width: 10%; ">&nbsp;</th>
-         <th class="label" style="width: 25%;">Name</th>
-         <th class="label" style="width: 65%;">Description</th>
+        <th class="label" style="width: 10%;">&nbsp;</th>
+        <th class="label" style="width: 25%;">Name</th>
+        <th class="label" style="width: 65%;">Description</th>
       </tr>
       <?php
-      $greenbar = '';
-      foreach ($all_roles as $role) {
-         if ($greenbar)
+        $greenbar = '';
+        foreach ($all_roles as $role) {
+          if ($greenbar)
             $greenbar = '';
-         else
+          else
             $greenbar = ' greenbar';
-         ?>
-         <tr>
-            <td class="value<?= $greenbar ?>"><input type="checkbox" name="role[<?= $role->id ?>]" value="1" <?php if ($customer->has_role($role->name)) print " CHECKED"; ?> /></td>
-            <td class="value<?= $greenbar ?>"><?= $role->name ?></td>
-            <td class="value<?= $greenbar ?>"><?= $role->description ?></td>
-         </tr>
-         <?php
-      }
-      ?>
-   </table>
+          ?>
+        <tr>
+          <td class="value<?= $greenbar ?>"><input type="checkbox" name="role[<?= $role->id ?>]" value="1" <?php if ($customer->has_role($role->name)) print " CHECKED"; ?> /></td>
+          <td class="value<?= $greenbar ?>"><?= $role->name ?></td>
+          <td class="value<?= $greenbar ?>"><?= $role->description ?></td>
+        </tr>
+      <?php } ?>
+    </table>
+
+<!-- ============================================== -->
+<!-- ASSIGNED ROLES OPT 2 -->
+<!-- ============================================== -->
+    <h3>Assigned Roles</h3>
+    <div class="tableBody min-tablet">
+      <div class="tableRowHeader">
+        <div class="tableCell" style="width: 20%;">Select</div>
+        <div class="tableCell" style="width: 25%;">Name</div>
+        <div class="tableCell" style="width: 30%;">Description</div>
+      </div>
+      <?php foreach ($contacts as $contact) { ?>
+      <div class="tableRow">
+        <div class="tableCell"><input type="checkbox" name="role[<?= $role->id ?>]" value="1" <?php if ($customer->has_role($role->name)) print " CHECKED"; ?> /></div>
+        <div class="tableCell"><?= $role->name ?></div>
+        <div class="tableCell"><?= $role->description ?></div>
+      </div>
+      <?php } ?>
+    </div>
+
+<!-- ============================================== -->
+<!-- AUTH FAILURES -->
+<!-- ============================================== -->
    <h3>Recent Auth Failures</h3>
 
     <section id="form-message">
