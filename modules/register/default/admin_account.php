b2e516f9fb9811f2a62cb79246f56a26cd2a2fcd
Chris Caravello <ccaravello@spectrosinstruments.com>
2023-07-05 21:11:31 +0100

    add pagination

--- modules/register/default/admin_account.php
+++ modules/register/default/admin_account.php
@@ -306,6 +306,17 @@
       </div>
    </div>
 
+   <!--    Standard Page Navigation Bar ADMIN ONLY -->
+   <div class="pager_bar">
+	    <div class="pager_controls">
+		    <a href="/_register/accounts?start=0&hidden=<?=$_REQUEST['hidden']?>&deleted=<?=$_REQUEST['deleted']?>&expired=<?=$_REQUEST['expired']?>" class="pager pagerFirst"><< First </a>
+		    <a href="/_register/accounts?start=<?=$prev_offset?>&hidden=<?=$_REQUEST['hidden']?>&deleted=<?=$_REQUEST['deleted']?>&expired=<?=$_REQUEST['expired']?>" class="pager pagerPrevious"><</a>
+		    &nbsp;<?=$_REQUEST['start']+1?> - <?=$_REQUEST['start']+$customers_per_page+1?> of <?=$total_customers?>&nbsp;
+		    <a href="/_register/accounts?start=<?=$next_offset?>&hidden=<?=$_REQUEST['hidden']?>&deleted=<?=$_REQUEST['deleted']?>&expired=<?=$_REQUEST['expired']?>" class="pager pagerNext">></a>
+		    <a href="/_register/accounts?start=<?=$last_offset?>&hidden=<?=$_REQUEST['hidden']?>&deleted=<?=$_REQUEST['deleted']?>&expired=<?=$_REQUEST['expired']?>" class="pager pagerLast"> Last >></a>
+	    </div>
+    </div>
+
 	<nav class="pageNav">
 		<a href="javascript:void(0)" class="pager pagerFirst" onclick="submitSearch(0)"><< <span class="hiddenMobile">First</span> </a>
 		<a href="javascript:void(0)" class="pager pagerPrevious" onclick="submitSearch(<?=$prev_offset?>)"><</a>
