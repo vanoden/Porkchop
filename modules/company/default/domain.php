6ab803abf2c04af0490b7919524c3654cb98a44b
Chris Caravello <ccaravello@spectrosinstruments.com>
2023-07-20 02:05:18 +0100

    refactor domain

--- modules/company/default/domain.php
+++ modules/company/default/domain.php
@@ -1,43 +1,32 @@
-<span class="title"><?=$domain_name?></span>
-
-<?php if ($page->errorCount() > 0) { ?>
-<section id="form-message">
-	<ul class="connectBorder errorText">
-		<li><?=$page->errorString()?></li>
-	</ul>
-</section>
-
-<?php	} else if ($page->success) { ?>
-<section id="form-message">
-	<ul class="connectBorder progressText">
-		<li><?=$page->success?></li>
-	</ul>
-</section>
-<?php	} ?>
+<!-- Page Header -->
+<?= $page->showTitle() ?>
+<?=$page->showBreadcrumbs()?>
+<?=$page->showMessages()?>
+<!-- End Page Header -->
+<h2><?=$domain_name?></h2>
 
 <form name="domain" method="post" action="/_company/domain">
-<input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
-<input type="hidden" name="id" value="<?=$domain->id?>">
-<div class="label">Name</div>
-<input type="text" name="domain_name" class="value input" value="<?=$domain->name?>"/>
-<div class="label">Registrar</div>
-<input type="text" name="domain_registrar" class="value input" value="<?=$domain->registrar?>"/>
-<div class="label">Registered</div>
-<input type="text" name="date_registered" class="value input" value="<?=$domain->date_registered?>"/>
-<div class="label">Expires</div>
-<input type="text" name="date_expires" class="value input" value="<?=$domain->date_expires?>"/>
-<div class="label">Company</div>
-<select name="company_id" class="value input">
-<?php	foreach ($companies as $company) { ?>
-	<option value="<?=$company->id?>"<?php if ($company->id == $domain->company->id) print " selected";?>><?=$company->name?></option>
-<?php	} ?>
-</select>
-<div class="label">Location</div>
-<select name="location_id" class="value input">
-<?php	foreach ($locations as $location) { ?>
-	<option value="<?=$location->id?>"<?php if ($location->id == $domain->location()->id) print " selected";?>><?=$location->name?></option>
-<?php	} ?>
-</select>
-<hr>
-<input type="submit" name="btn_submit" value="Submit" class="button"/>
+  <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
+  <input type="hidden" name="id" value="<?=$domain->id?>">
+  <div class="label">Name</div>
+  <input type="text" name="domain_name" class="value input" value="<?=$domain->name?>"/>
+  <div class="label">Registrar</div>
+  <input type="text" name="domain_registrar" class="value input" value="<?=$domain->registrar?>"/>
+  <div class="label">Registered</div>
+  <input type="text" name="date_registered" class="value input" value="<?=$domain->date_registered?>"/>
+  <div class="label">Expires</div>
+  <input type="text" name="date_expires" class="value input" value="<?=$domain->date_expires?>"/>
+  <div class="label">Company</div>
+  <select name="company_id" class="value input">
+  <?php	foreach ($companies as $company) { ?>
+    <option value="<?=$company->id?>"<?php if ($company->id == $domain->company->id) print " selected";?>><?=$company->name?></option>
+  <?php	} ?>
+  </select>
+  <div class="label">Location</div>
+  <select name="location_id" class="value input">
+  <?php	foreach ($locations as $location) { ?>
+    <option value="<?=$location->id?>"<?php if ($location->id == $domain->location()->id) print " selected";?>><?=$location->name?></option>
+  <?php	} ?>
+  </select>
+  <input type="submit" name="btn_submit" value="Submit" class="button"/>
 </form>
