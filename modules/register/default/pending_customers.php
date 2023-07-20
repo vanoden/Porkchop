9c1129b9c7345e28e772168f50ebe39c61e2e125
Chris Caravello <ccaravello@spectrosinstruments.com>
2023-07-20 02:03:43 +0100

    refactor pending customers

--- modules/register/default/pending_customers.php
+++ modules/register/default/pending_customers.php
@@ -156,200 +156,171 @@
 	   });
    }
 </script>
-<div class="breadcrumbs">
-   <a href="/_support/requests">Support Home</a> &gt; Customer Requests
-</div>
-<h2 style="display: inline-block;"><i class='fa fa-users' aria-hidden='true'></i> Customer Registrations </h2>
-<?php include(MODULES.'/register/default/partials/search_bar.php'); ?>
-<div style="width: 100%;">
-   <?php	if ($page->errorCount()) { ?>
-       <div class="form_error"><?=$page->errorString()?></div>
-   <?php } elseif ($page->success) { ?>
-        <div class="form_success"><?=$page->success?></div>
-   <?php } ?>
-   <form action="/_register/pending_customers" method="post" autocomplete="off">
-      <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
-	  <table>
-		 <tr>
-			<th><span class="label"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Start Date</th>
-			<th><span class="label"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> End Date</th>
-			<th><span class="label"><i class="fa fa-filter" aria-hidden="true"></i> Status</span></th>
-		 </tr>
-		 <tr>
-			<td><input type="text" id="dateStart" name="dateStart" class="value input" value="<?=isset($_REQUEST['fromDate']) ? $_REQUEST['fromDate'] : ''?>" /></td>
-			<td><input type="text" id="dateEnd" name="dateEnd" class="value input" value="<?=isset($_REQUEST['toDate']) ? $_REQUEST['toDate'] : ''?>" /></td>
-			<td style="width: 50%;">
-			   <?php foreach ($possibleStatii as $possibleStatus) { ?>
-			   <input type="checkbox" name="<?=$possibleStatus?>" value="<?=$possibleStatus?>"
-				  <?php
-					 if (isset($_REQUEST[$possibleStatus]) && $_REQUEST[$possibleStatus] == $possibleStatus) print " checked"; 
-					 ?> /><?=$possibleStatus?>
-			   <?php } ?>
-			</td>
-		 </tr>
-	  </table>
-	  <input type="submit" name="btn_submit" class="button" value="Filter Results" style="float:right;" /><br/><br/>
-   </form>
-</div>
+
+<!-- Page Header -->
+<?= $page->showTitle() ?>
+<?=$page->showBreadcrumbs()?>
+<?=$page->showMessages()?>
+<!-- End Page Header -->
+
+<form action="/_register/pending_customers" method="post" autocomplete="off">
+  <div id="search_container">
+    <div><label>Start </label><input type="text" id="dateStart" name="dateStart" placeholder="choose date" value="<?=isset($_REQUEST['fromDate']) ? $_REQUEST['fromDate'] : ''?>" /></div>
+    <div><label>End </label><input type="text" id="dateEnd" name="dateEnd" placeholder="choose date" value="<?=isset($_REQUEST['toDate']) ? $_REQUEST['toDate'] : ''?>" /></div>
+    <div>
+      <?php foreach ($possibleStatii as $possibleStatus) { ?>
+        <input type="checkbox" name="<?=$possibleStatus?>" value="<?=$possibleStatus?>"
+        <?php
+          if (isset($_REQUEST[$possibleStatus]) && $_REQUEST[$possibleStatus] == $possibleStatus) print " checked"; 
+        ?> /><?=$possibleStatus?>
+      <?php } ?>
+    </div>
+    <input type="submit" name="btn_submit" class="button" value="Filter" />
+  </div>
+</form>
+
 <?php
-   if ($page->success) {
-   ?>
-<h3 class="success-message"><i class="fa fa-check-square-o" aria-hidden="true"></i> Customer Updated</h3>
+  if ($page->success) {
+?>
+  <h3 class="success-message"><i class="fa fa-check-square-o" aria-hidden="true"></i> Customer Updated</h3>
 <?php
-   }
-   if ($page->error) {
-   ?>
-<h4 class="error-message"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Error has occurred updating customer</h4>
+  }
+  if ($page->error) {
+  ?>
+  <h4 class="error-message"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Error has occurred updating customer</h4>
 <?php
-   }
-   ?>
-<h2 style="display: inline-block;">Pending Customers
+}
+?>
+
+<h3>Pending Customers
    <?=isset($page->isSearchResults) ? "[Found Customers: ". count($queuedCustomersList)."]" : "";?>
-</h2>
+</h3>
 <!--	START First Table -->
-<div class="tableBody" style="min-width: 100%;">
-   <div class="tableRowHeader">
-	  <div class="tableCell">Organization</div>
-	  <div class="tableCell">Customer</div>
-	  <div class="tableCell">Status</div>
-	  <div class="tableCell">Date</div>
-	  <div class="tableCell">Address</div>
-	  <div class="tableCell">Contact</div>
-	  <div class="tableCell">Product</div>
-	  <div class="tableCell">Admin Notes</div>
-   </div>
-   <?php
+<div class="tableBody">
+  <div class="tableRowHeader">
+    <div class="tableCell">Organization</div>
+    <div class="tableCell">Customer</div>
+    <div class="tableCell">Status</div>
+    <div class="tableCell">Date</div>
+    <div class="tableCell">Address</div>
+    <div class="tableCell">Contact</div>
+    <div class="tableCell">Product</div>
+    <div class="tableCell">Admin Notes</div>
+  </div>
+  <?php
 	  foreach ($queuedCustomersList as $queuedCustomer) {      
 			$registerCustomer = $queuedCustomer->customer();  
 			$productItem = new \Product\Item($queuedCustomer->product_id);
 			$phone = $registerCustomer->contacts(array('type' => 'phone'))[0];
 			$email = $registerCustomer->contacts(array('type' => 'email'))[0];
-	  ?>
+	?>
 	<div class="tableRow">
 		<div class="tableCell"><?=$queuedCustomer->name?>
-<?php	if ($queuedCustomer->is_reseller) { ?>&nbsp;[Reseller]<?php	} ?>
-		<br>
-		<form method="POST" id="customer_add_form_<?=$queuedCustomer->id?>" action="/_register/pending_customers?search=<?=$_REQUEST['search']?>">
+      <?php	if ($queuedCustomer->is_reseller) { ?>&nbsp;[Reseller]<?php	} ?>
+		  <form method="POST" id="customer_add_form_<?=$queuedCustomer->id?>" action="/_register/pending_customers?search=<?=$_REQUEST['search']?>">
 		    <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
-		<?php
-			  switch ($queuedCustomer->status) {
-				  case 'PENDING':
-					  ?>
-			<div class="ui-widget">
-				<label for="organization">Match Organization: </label>
-				<input class="organization" id="organization_<?=$queuedCustomer->id?>" name="organization" value="<?=$queuedCustomer->name?>"/><br>
-				<button type="button" disabled="disabled" id="organization_<?=$queuedCustomer->id?>_assign_button" onclick="assignCustomer(<?=$queuedCustomer->id?>)"><i class="fa fa-check-circle" aria-hidden="true"></i> Assign Existing</button>
-				<button type="button" disabled="disabled" id="organization_<?=$queuedCustomer->id?>_new_button" onclick="assignCustomer(<?=$queuedCustomer->id?>)"><i class="fa fa-plus" aria-hidden="true"></i> Add as New</button>
-				<button type="button" id="organization_<?=$queuedCustomer->id?>_deny_button" onclick="denyCustomer(<?=$queuedCustomer->id?>)"><i class="fa fa-ban" aria-hidden="true"></i> Deny</button>
-			</div>
-		   <?php
-			  break;
-			  case 'VERIFYING':
-			  ?>
-		   <span style="color: <?=colorCodeStatus("VERIFYING")?>">
-    		   <i class="fa fa-clock-o" aria-hidden="true"></i> email validating
-    		   <br/>
-    		   <button type="button" class="resend-verify-message" onclick="resend(<?=$queuedCustomer->register_user_id?>)">Resend Verify Email Message</button>
-		   </span>
-		   <?php
-			  break;
-			  case 'APPROVED':
-				 ?>
-		   <span style="color: <?=colorCodeStatus("APPROVED")?>">
-		   <i class="fa fa-check-circle" aria-hidden="true"></i> approved
-		   </span>
-		   <?php
-			  break;
-			  default:
-			  ?>
-		   <span style="color: <?=colorCodeStatus("DENIED")?>">
-		   <i class="fa fa-times" aria-hidden="true"></i> denied
-		   </span>
-		   <?php
-			  break;
-			  }
-			  ?>
-		   <input id="customer_add_<?=$queuedCustomer->id?>" type="hidden" name="action" value="assignCustomer"/>
-		   <input type="hidden" name="id" value="<?=$queuedCustomer->id?>"/>
-		</form>
+        <?php
+          switch ($queuedCustomer->status) {
+          case 'PENDING':
+        ?>
+        <div class="ui-widget">
+          <label for="organization">Match Organization: </label>
+          <input class="organization" id="organization_<?=$queuedCustomer->id?>" name="organization" value="<?=$queuedCustomer->name?>"/><br>
+          <button type="button" disabled="disabled" id="organization_<?=$queuedCustomer->id?>_assign_button" onclick="assignCustomer(<?=$queuedCustomer->id?>)"><i class="fa fa-check-circle" aria-hidden="true"></i> Assign Existing</button>
+          <button type="button" disabled="disabled" id="organization_<?=$queuedCustomer->id?>_new_button" onclick="assignCustomer(<?=$queuedCustomer->id?>)"><i class="fa fa-plus" aria-hidden="true"></i> Add as New</button>
+          <button type="button" id="organization_<?=$queuedCustomer->id?>_deny_button" onclick="denyCustomer(<?=$queuedCustomer->id?>)"><i class="fa fa-ban" aria-hidden="true"></i> Deny</button>
+        </div>
+        <?php
+          break;
+          case 'VERIFYING':
+        ?>
+		    <span style="color: <?=colorCodeStatus("VERIFYING")?>"><i class="fa fa-clock-o" aria-hidden="true"></i> email validating
+    	  <button type="button" class="resend-verify-message" onclick="resend(<?=$queuedCustomer->register_user_id?>)">Resend Verify Email Message</button></span>
+        <?php
+          break;
+          case 'APPROVED':
+        ?>
+		    <span style="color: <?=colorCodeStatus("APPROVED")?>"><i class="fa fa-check-circle" aria-hidden="true"></i> approved</span>
+        <?php
+          break;
+          default:
+        ?>
+		    <span style="color: <?=colorCodeStatus("DENIED")?>"><i class="fa fa-times" aria-hidden="true"></i> denied</span>
+        <?php
+          break;
+          }
+        ?>
+		    <input id="customer_add_<?=$queuedCustomer->id?>" type="hidden" name="action" value="assignCustomer"/>
+		    <input type="hidden" name="id" value="<?=$queuedCustomer->id?>"/>
+		  </form>
 		</div>
 	  <div class="tableCell">
-		 <?=$registerCustomer->first_name?> <?=$registerCustomer->last_name?>
-		 <br/>
-		 <strong>Login:</strong> <i><?=$registerCustomer->login?></i>
+		  <?=$registerCustomer->first_name?> <?=$registerCustomer->last_name?>
+		  <br/>
+		  <strong>Login:</strong> <i><?=$registerCustomer->login?></i>
 	  </div>
 	  <div class="tableCell">
-		 <div id="customer_status_form_<?=$queuedCustomer->id?>" class="hidden customer_status_form">
-			<form method="POST" action="/_register/pending_customers?search=<?=$_REQUEST['search']?>">
-     		  <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
-			   Update Status:<br/>
-			   <select name="status">
-				  <?php foreach ($possibleStatii as $possibleStatus) { ?>
-				  <option value="<?=$possibleStatus?>" <?=($queuedCustomer->status == $possibleStatus) ? 'selected="selected"' : ""?>><?=$possibleStatus?></option>
-				  <?php } ?>
-			   </select>
-			   <br/>
-			   <input type="hidden" name="action" value="updateStatus"/>
-			   <input type="hidden" name="id" value="<?=$queuedCustomer->id?>"/>
-			   <button type="submit">Save</button>
-			   <button type="button" onclick="cancelEditStatus(<?=$queuedCustomer->id?>)">Cancel</button>
-			</form>
-		 </div>
-		 <div id="customer_status_form_links_<?=$queuedCustomer->id?>" class="customer_status_form_links">		 
-			<br/>   
-			<span style="color: <?=colorCodeStatus($queuedCustomer->status)?>">
-			<?=$queuedCustomer->status?>
-			</span>
-			<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			<a class="small-text cursor-pointer" onclick="editStatus(<?=$queuedCustomer->id?>)"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</a>
-			<br/><br/>
-		 </div>
+		  <div id="customer_status_form_<?=$queuedCustomer->id?>" class="hidden customer_status_form">
+		    <form method="POST" action="/_register/pending_customers?search=<?=$_REQUEST['search']?>">
+     		  <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">Update Status:<br/>
+			    <select name="status">
+				    <?php foreach ($possibleStatii as $possibleStatus) { ?>
+				      <option value="<?=$possibleStatus?>" <?=($queuedCustomer->status == $possibleStatus) ? 'selected="selected"' : ""?>><?=$possibleStatus?></option>
+				    <?php } ?>
+			    </select>
+			    <input type="hidden" name="action" value="updateStatus"/>
+			    <input type="hidden" name="id" value="<?=$queuedCustomer->id?>"/>
+			    <button type="submit">Save</button>
+			    <button type="button" onclick="cancelEditStatus(<?=$queuedCustomer->id?>)">Cancel</button>
+			  </form>
+		  </div>
+		  <div id="customer_status_form_links_<?=$queuedCustomer->id?>" class="customer_status_form_links">
+			  <span style="color: <?=colorCodeStatus($queuedCustomer->status)?>"><?=$queuedCustomer->status?></span>
+			  <a class="small-text cursor-pointer" onclick="editStatus(<?=$queuedCustomer->id?>)"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</a>
+		  </div>
 	  </div>
 	  <div class="tableCell">
-		 <?=date("F j, Y, g:i a", strtotime($queuedCustomer->date_created))?>
+		  <?=date("F j, Y, g:i a", strtotime($queuedCustomer->date_created))?>
 	  </div>
 	  <div class="tableCell">
-		 <?=$queuedCustomer->address?><br/>
-		 <?=$queuedCustomer->city?>,
-		 <?=$queuedCustomer->state?>
-		 <?=$queuedCustomer->zip?>
+		  <?=$queuedCustomer->address?><br/>
+		  <?=$queuedCustomer->city?>,
+		  <?=$queuedCustomer->state?>
+		  <?=$queuedCustomer->zip?>
 	  </div>
 	  <div class="tableCell">
-		 <?php if (isset($phone->value)) { ?>
-			 <strong>Phone:</strong> <?=$phone->value?><br/>
-		 <?php } ?>
-		 <?php if (isset($email->value)) { ?>
-			 <strong>Email:</strong> <?=$email->value?><br/>
-		 <?php } ?>
+		  <?php if (isset($phone->value)) { ?><strong>Phone:</strong> <?=$phone->value?><br/><?php } ?>
+		  <?php if (isset($email->value)) { ?>
+			  <strong>Email:</strong> <?=$email->value?><br/>
+		  <?php } ?>
 	  </div>
 	  <div class="tableCell">
-		 <?php if ($queuedCustomer->product_id) { ?>
-			 <div style="color: #003308; padding: 10px 0px 10px 0px;"><?=$productItem->name?> [<?=$productItem->code?>]</div>
-			 <strong>Serial #:</strong> <?=$queuedCustomer->serial_number?><br/>
-		 <?php } ?>
-		 <br/>
+		  <?php if ($queuedCustomer->product_id) { ?>
+			  <div style="color: #003308; padding: 10px 0px 10px 0px;"><?=$productItem->name?> [<?=$productItem->code?>]</div>
+			  <strong>Serial #:</strong> <?=$queuedCustomer->serial_number?><br/>
+      <?php } ?>
 	  </div>
 	  <div class="tableCell">
-		 <div id="customer_notes_form_<?=$queuedCustomer->id?>" class="hidden customer_notes_form">
-			<form method="POST" action="/_register/pending_customers?search=<?=$_REQUEST['search']?>">
-   			   <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
-			   <input type="text" name="notes" value="<?=$queuedCustomer->notes?>"/><br/>
-			   <input type="hidden" name="action" value="updateNotes"/>
-			   <input type="hidden" name="id" value="<?=$queuedCustomer->id?>"/>
-			   <button type="submit">Save</button>
-			   <button type="button" onclick="cancelEditNote(<?=$queuedCustomer->id?>)">Cancel</button>
-			</form>
-		 </div>
-		 <div id="customer_notes_edit_links_<?=$queuedCustomer->id?>" class="customer_notes_edit_links">
-			<?=$queuedCustomer->notes?> <br/><br/>
-			<?php if ($queuedCustomer->notes) { ?>
-			<a class="small-text cursor-pointer" onclick="editNote(<?=$queuedCustomer->id?>)"><i class="fa fa-pencil-square" aria-hidden="true"></i> Edit Note</a>
-			<?php } else { ?>
-			<a class="small-text cursor-pointer" onclick="editNote(<?=$queuedCustomer->id?>)"><i class="fa fa-plus-square" aria-hidden="true"></i> Add Note</a>
-			<?php } ?>
-		 </div>
+		  <div id="customer_notes_form_<?=$queuedCustomer->id?>" class="hidden customer_notes_form">
+        <form method="POST" action="/_register/pending_customers?search=<?=$_REQUEST['search']?>">
+          <input type="hidden" name="csrfToken" value="<?=$GLOBALS['_SESSION_']->getCSRFToken()?>">
+          <input type="text" name="notes" value="<?=$queuedCustomer->notes?>"/><br/>
+          <input type="hidden" name="action" value="updateNotes"/>
+          <input type="hidden" name="id" value="<?=$queuedCustomer->id?>"/>
+          <button type="submit">Save</button>
+          <button type="button" onclick="cancelEditNote(<?=$queuedCustomer->id?>)">Cancel</button>
+        </form>
+		  </div>
+		  <div id="customer_notes_edit_links_<?=$queuedCustomer->id?>" class="customer_notes_edit_links">
+		    <?=$queuedCustomer->notes?> <br/><br/>
+			  <?php if ($queuedCustomer->notes) { ?>
+			    <a class="small-text cursor-pointer" onclick="editNote(<?=$queuedCustomer->id?>)"><i class="fa fa-pencil-square" aria-hidden="true"></i> Edit Note</a>
+			  <?php } else { ?>
+			    <a class="small-text cursor-pointer" onclick="editNote(<?=$queuedCustomer->id?>)"><i class="fa fa-plus-square" aria-hidden="true"></i> Add Note</a>
+			  <?php } ?>
+		  </div>
 	  </div>
-   </div>
-   <?php	} ?>
+  </div>
+  <?php	} ?>
 </div>
 <!--	END First Table -->
