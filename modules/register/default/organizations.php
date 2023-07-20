dc60b17659d27f66b35d221022a863bb089e6876
Chris Caravello <ccaravello@spectrosinstruments.com>
2023-07-20 02:04:03 +0100

    refactor organizations

--- modules/register/default/organizations.php
+++ modules/register/default/organizations.php
@@ -17,59 +17,63 @@
 </style>
 
 <!-- Page Header -->
-<?=$page->showBreadcrumbs()?>
 <?=$page->showTitle()?>
+<?=$page->showBreadcrumbs()?>
 <?=$page->showMessages()?>
 <!-- End Page Header -->
 
-    <form id="orgSearch" method="get" class="float: left">
-        <div id="search_container">
-	        <input type="text" id="searchOrganizationInput" name="name" placeholder="organization name" value="<?php if (!empty($_REQUEST["name"])) print $_REQUEST["name"];?>" class="value input searchInput wide_md"/>
-	        <input type="checkbox" name="hidden" class="checkbox" value="1" <?php if (!empty($_REQUEST['hidden'])) print "checked"; ?> /><span class="status">Hidden</span>
-	        <input type="checkbox" name="expired" class="checkbox" value="1" <?php if (!empty($_REQUEST['expired'])) print "checked"; ?> /><span class="status">Expired</span>
-	        <input type="checkbox" name="deleted" class="checkbox" value="1" <?php if (!empty($_REQUEST['deleted'])) print "checked"; ?> /><span class="status">Deleted</span>
-	        <input type="hidden" id="start" name="start" value="0">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Filter by Tag:
-			<select name="searchedTag" id="organizationStatusValue" class="">
-                <option value=""></option>
-	            <?php		foreach ($organizationTags as $tag) { print_r($tag); ?>
-	            <option value="<?=$tag?>"<?php	if ($tag == $_REQUEST['searchedTag']) print " selected"; ?>><?=$tag?></option>
-	            <?php		} ?>
-            </select>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Records per page:
-			<input type="text" name="page_size" class="value input" style="width: 45px" value="<?=$organizations_per_page?>" />
-			&nbsp;&nbsp;&nbsp;&nbsp;
-			<button id="searchOrganizationButton" name="btn_search" onclick="submitSearch(0)"/>Search</button>
-        </div>
-        <hr style="visibility: hidden">
-        <table cellpadding="0" cellspacing="0" class="body">
-            <tr><th class="label organizationsCodeLabel">ID</th>
-	            <th class="label organizationsNameLabel">Name</th>
-	            <th class="label organizationsCodeLabel">Status</th>
-	            <th class="label organizationsCodeLabel">Members</th>
-	            <th class="label organizationsCodeLabel">Devices</th>
-            </tr>
-            <?php
-            foreach ($organizations as $organization) { 
-                if (isset($greenbar)) $greenbar = ''; else $greenbar = " greenbar";
-            ?>
-            <tr><td class="value<?=$greenbar?>"><a class="value<?=$greenbar?>" href="<?=PATH."/_register/organization?organization_id=".$organization->id?>"><?=$organization->code?></a></td>
-	            <td class="value<?=$greenbar?>"><?=$organization->name?></td>
-	            <td class="value<?=$greenbar?>"><?=$organization->status?></td>
-	            <td class="value<?=$greenbar?>"><?=$organization->activeHumans()?></td>
-	            <td class="value<?=$greenbar?>"><?=$organization->activeDevices()?></td>
-            </tr>
-            <?php
-            }
-            if (!is_array($organizations) || !count($organizations)) {
-            ?>
-                <tr>
-	                <td colspan="5"><p>No Organizations Found</p></td>
-                </tr>
-            <?php
-            }
-            ?>
-        </table>
+<form id="orgSearch" method="get" class="float: left">
+  <div id="search_container">
+    <div><input type="text" id="searchOrganizationInput" name="name" placeholder="organization name" value="<?php if (!empty($_REQUEST["name"])) print $_REQUEST["name"];?>"/></div>
+    <div><input type="checkbox" name="hidden" class="checkbox" value="1" <?php if (!empty($_REQUEST['hidden'])) print "checked"; ?> /><label>Hidden</label></div>
+    <div><input type="checkbox" name="expired" class="checkbox" value="1" <?php if (!empty($_REQUEST['expired'])) print "checked"; ?> /><label>Expired</label></div>
+    <div><input type="checkbox" name="deleted" class="checkbox" value="1" <?php if (!empty($_REQUEST['deleted'])) print "checked"; ?> /><label>Deleted</label></div>
+    <div><label></label><input type="hidden" id="start" name="start" value="0"></div>
+    <div>
+      <label>Filter by Tag:</label>
+      <select name="searchedTag" id="organizationStatusValue" class="">
+        <option value="Choose tag"></option>
+          <?php		foreach ($organizationTags as $tag) { print_r($tag); ?>
+          <option value="<?=$tag?>"<?php	if ($tag == $_REQUEST['searchedTag']) print " selected"; ?>><?=$tag?>
+          </option>
+          <?php		} ?>
+      </select>
+    </div>
+    <div><label>Records per page:</label><input type="text" name="page_size" class="value input" style="width: 45px" value="<?=$organizations_per_page?>" /></div>
+    <button id="searchOrganizationButton" name="btn_search" onclick="submitSearch(0)"/>Search</button>
+  </div>
+
+  <div class="tableBody">
+      <div class="tableRowHeader">
+        <div class="tableCell">ID</div>
+        <div class="tableCell">Name</div>
+        <div class="tableCell">Status</div>
+        <div class="tableCell">Members</div>
+        <div class="tableCell">Devices</div>
+      </div>
+      <?php
+      foreach ($organizations as $organization) { 
+          if (isset($greenbar)) $greenbar = ''; else $greenbar = " greenbar";
+      ?>
+      <div class="tableRow">
+        <div class="tableCell"><a href="<?=PATH."/_register/organization?organization_id=".$organization->id?>"><?=$organization->code?></a></div>
+        <div class="tableCell"><?=$organization->name?></div>
+        <div class="tableCell"><?=$organization->status?></div>
+        <div class="tableCell"><?=$organization->activeHumans()?></div>
+        <div class="tableCell"><?=$organization->activeDevices()?></div>
+      </div>
+      <?php
+      }
+      if (!is_array($organizations) || !count($organizations)) {
+      ?>
+          <div class="tableRow">
+            <td colspan="5"><p>No Organizations Found</p></td>
+      </div>
+      <?php
+      }
+      ?>
+  </div><!-- end table -->
+
         <!--    Standard Page Navigation Bar ADMIN ONLY -->
         <div class="pager_bar">
 	        <div class="pager_controls">
