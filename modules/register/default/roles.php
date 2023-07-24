8d1384740fa5a6ca35a71c78aa7fd945fb509ec4
Chris Caravello <ccaravello@spectrosinstruments.com>
2023-07-24 15:38:47 +0100

    refactored roles page

--- modules/register/default/roles.php
+++ modules/register/default/roles.php
@@ -10,13 +10,13 @@
 	<div class="tableRowHeader">
 		<div class="tableCell">Name</div>
 		<div class="tableCell">Description</div>
-		<div class="tableCell">Remove?</div>		
+		<div class="tableCell" style="text-align: center;">Remove?</div>		
 	</div>
 <?php	foreach ($roles as $role) { ?>
 	<div class="tableRow">
 		<div class="tableCell"><a href="/_register/role/<?=$role->name?>"><?=$role->name?></a></div>
 		<div class="tableCell"><?=$role->description?></div>
-		<div class="tableCell"><a href="/_register/roles?remove_id=<?=$role->id?>"><img src="/img/icons/icon_tools_trash_active.svg" style="cursor: pointer; width: 20px; border: none;" alt="delete role"></a></div>
+		<div class="tableCell" style="text-align: center;"><a href="/_register/roles?remove_id=<?=$role->id?>"><img src="/img/icons/icon_tools_trash_active.svg" style="cursor: pointer; width: 20px; border: none;" alt="delete role"></a></div>
 	</div>
 <?php	} ?>
 </div>
