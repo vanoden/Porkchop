32caac11f62ca12e942b45dde20876aa1a4ed031
Anthony Caravello <tony@caravello.us>
2023-08-31 12:00:39 -0400

    Fixed event_type/event_class conflict in Register::UserAuditEvent

--- classes/Register/UserAuditEvent.php
+++ classes/Register/UserAuditEvent.php
@@ -6,13 +6,13 @@
 		public $user_id;
 		public $admin_id;
 		public $event_date;
-		public $event_type;
+		public $event_class;
 		public $event_notes;
 
 	    public function __construct(int $id = null) {
 			$this->_tableName = "register_user_audit";
 			$this->_tableUKColumn = null;
-			$this->_addFields("user_id", "admin_id", "event_date", "event_type", "event_notes");
+			$this->_addFields("user_id", "admin_id", "event_date", "event_class", "event_notes");
             parent::__construct($id);
 		}
 	}
