d1a192da412d4663be732fcf2134a72e853d30b0
Anthony Caravello <tony@caravello.us>
2024-01-04 12:10:47 -0500

    Removed engine settings from new version of Register::Schema

--- classes/Register/Schema.php
+++ classes/Register/Schema.php
@@ -1139,7 +1139,7 @@
 					KEY `idx_register_organization_audit_class` (`event_class`),
 					CONSTRAINT `register_organization_audit_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `register_organizations` (`id`),
 					CONSTRAINT `register_organization_audit_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `register_users` (`id`)
-				  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+				  );
 				";
 
 				$GLOBALS['_database']->Execute($create_table_query);
