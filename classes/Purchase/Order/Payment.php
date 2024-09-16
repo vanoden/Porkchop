db30622d7fb717bc1ffb7996742259d1d5b2d1a1
Anthony Caravello <tony@caravello.us>
2024-09-16 08:36:01 -0400

    Updated Purchase classes for BaseModel

--- classes/Purchase/Order/Payment.php
+++ classes/Purchase/Order/Payment.php
@@ -1,27 +1,12 @@
 <?php
 	namespace Purchase\Order;
 
-	class Payment {
-		public $id;
-		private $_error;
+	class Payment extends \BaseModel {
 
 		public function __construct($id = null) {
 			if (!empty($id)) {
 				$this->id = $id;
 				return $this->details();
 			}
-		}
-
-		public function add($params = array()) {
-			return $this->update($params);
-		}
-
-		public function update($params = array()) {
-			return $this->details();
-		}
-
-		public function error($error = null) {
-			if (!empty($error)) $this->_error = $error;
-			return $this->_error;
 		}
 	}
