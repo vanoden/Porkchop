e6bb9ae5444ddf3b7f29340bd1cb602212c5eceb
Chris Caravello <ccaravello@spectrosinstruments.com>
2025-01-22 17:28:58 -0500

    update the business card layout

--- modules/register/default/businesscard.php
+++ modules/register/default/businesscard.php
@@ -10,19 +10,19 @@
     <section class="vcard">
         <img class="vcard-logo" src="/img/vcard/logo_spectros.png" alt="Spectros Instruments logo">
         <img class="vcard-profile" src="/img/_global/icon_myaccount.svg" alt="personal profile photo">
-`        <h1><?= $customer->first_name . ' ' . $customer->last_name ?></h1>
+        <h1><?= $customer->first_name . ' ' . $customer->last_name ?></h1>
         <div class="about-us">
             <h2>About our company</h2>
             <p><?= $customer->organization()->name ?></p>
         </div>
-        <a class="vcard-button" href="/_register/businessvcard/<?= $customer->code ?>">Add to Contacts</a>
+        <a class="vcard-button" href="/business_card?vcard=show">Add to Contacts</a>
         <ul class="vcard-contact">
             <?php foreach ($contacts as $contact) : ?>
                 <?php if ($contact->public) : // Check if the contact is public ?>
                     <li>
+                      <a href="<?= $contact->type === 'phone' ? 'tel:' . $contact->value : ($contact->type === 'sms' ? 'sms:' . $contact->value : ($contact->type === 'email' ? 'mailto:' . $contact->value : '#')) ?>">
                         <img class="vcard-icon" src="/img/vcard/icon_vcard-<?= $contact->type ?>.png" alt="<?= $contact->type ?>">
-                        <a href="<?= $contact->type === 'phone' ? 'tel:' . $contact->value : ($contact->type === 'sms' ? 'sms:' . $contact->value : ($contact->type === 'email' ? 'mailto:' . $contact->value : '#')) ?>">
-                            <?= htmlspecialchars($contact->value) ?>
+                            <!-- <?= htmlspecialchars($contact->value) ?> -->
                         </a>
                     </li>
                 <?php endif; ?>
