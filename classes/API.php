a94dfcf1d75dd754cde8b1594d91eb322dacb536
Anthony Caravello <tony@caravello.us>
2025-02-03 17:27:11 -0500

    added 'role_required' option, content types and support for content_type in addition to content-type, now depracated

--- classes/API.php
+++ classes/API.php
@@ -314,6 +314,9 @@
 			if (isset($method['privilege_required']) && !(preg_match('/^\[\w+\]$/',$method['privilege_required']))) {
 				$this->requirePrivilege($method['privilege_required']);
 			}
+			if (isset($method['role_required']) && !(preg_match('/^\[\w+\]$/',$method['role_required']))) {
+				$this->requireRole($method['role_required']);
+			}
 			if (isset($method['authentication_required']) && $method['authentication_required']) {
 				$this->requireAuth();
 			}
@@ -350,20 +353,33 @@
 					}
 				}
 				// Enforce Parameter Type Requirements
-				if (!empty($value) && isset($options['content-type'])) {
-					//print_r("\t".$options['content-type']."\n");
-					if (in_array($options['content-type'],['int','integer','float']) && ! is_numeric($value)) {
+				if (!empty($value) && (isset($options['content-type']) || isset($options['content_type']))) {
+					// Because I was stupid and used content-type instead of content_type at first
+					if (isset($options['content-type']) && !isset($options['content_type'])) $options['content_type'] = $options['content-type'];
+					if (in_array($options['content_type'],['int','integer','float']) && ! is_numeric($value)) {
 						$this->invalidRequest("Invalid $param value");
 					}
-					elseif (in_array($options['content-type'],['bool','boolean'])) {
+					elseif (in_array($options['content_type'],['bool','boolean'])) {
 						if ($_REQUEST[$param] == 1) $_REQUEST[$param] = 'true';
 						elseif ($_REQUEST[$param] == 0) $_REQUEST[$param] = 'false';
 						if ($_REQUEST[$param] != 'true' && $_REQUEST[$param] != 'false') {
 							$this->invalidRequest("Invalid $param value");
 						}
 					}
-					elseif (in_array($options['content-type'],['date','datetime'])) {
+					elseif (in_array($options['content_type'],['date','datetime'])) {
 						if (! get_mysql_date($value)) $this->invalidRequest("Invalid $param value");
+					}
+					elseif ($options['content_type'] == 'email') {
+						if (! filter_var($value,FILTER_VALIDATE_EMAIL)) $this->invalidRequest("Invalid $param value");
+					}
+					elseif ($options['content_type'] == 'url') {
+						if (! filter_var($value,FILTER_VALIDATE_URL)) $this->invalidRequest("Invalid $param value");
+					}
+					elseif ($options['content_type'] == 'phone') {
+						if (! preg_match('/^\d{10,11}$/',$value)) $this->invalidRequest("Invalid $param value");
+					}
+					elseif ($options['content_type'] == 'file') {
+						if (! preg_match('/^[\w\-\_\.]+$/',$value)) $this->invalidRequest("Invalid $param value");
 					}
 				}
 				// Enforce Parameter Regex Requirements
