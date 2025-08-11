fce045f15cc343e2efd30e0aa828a488e708725f
Anthony Caravello <tony@caravello.us>
2025-08-11 08:54:04 -0400

    properties for BaseXREF

--- classes/BaseXREF.php
+++ classes/BaseXREF.php
@@ -1,5 +1,7 @@
 <?php
 
 class BaseXREF Extends \BaseClass {
-	
+    protected int $id = 0; // AutoIncrementing Record ID
+	protected $_tableName;
+    protected array $_tableUKColumns = []; // Unique Key Columns
 }
