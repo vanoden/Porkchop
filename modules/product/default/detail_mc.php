4e6e20d6561f4f825e27e3c2f69ab03dc314628a
Chris Caravello <ccaravello@spectrosinstruments.com>
2025-09-23 17:37:33 +0000

    new product template

--- modules/product/default/detail_mc.php
+++ modules/product/default/detail_mc.php
@@ -1,25 +1,17 @@
 <?php
-	$page = new \Site\Page();
+	$site = new \Site();
+	$page = $site->page();
 
-	$productList = new \Product\ItemList();
-	$parent = new \Product\Item();
+	$product = new \Product\Item();
 	
-	if (! isset($_REQUEST['parent_code'])) $_REQUEST['parent_code'] = $GLOBALS['_REQUEST_']->query_vars_array[0];
+	if (! isset($_REQUEST['product_code'])) $_REQUEST['product_code'] = $GLOBALS['_REQUEST_']->query_vars_array[0];
 	
-	if (isset($_REQUEST['parent_code'])) {
-		if ($parent->validCode($_REQUEST['parent_code'])) {
-			$parent->get($_REQUEST['parent_code']);
-			$_REQUEST['parent_id'] = $parent->id;
+	if (isset($_REQUEST['product_code'])) {
+		if ($product->validCode($_REQUEST['product_code'])) {
+			$product->get($_REQUEST['product_code']);
+			$_REQUEST['product_id'] = $product->id;
 		}
 		else {
-			$page->addError("Invalid parent code");
+			$page->addError("Invalid product code");
 		}
 	}
-	
-	if (! $_REQUEST['parent_id']) {
-		$_REQUEST['parent_id'] = 0;
-		$parent->code = '';
-		$parent->name = "Our Products";
-	}
-	$products = $productList->find(array("parent_id" => $_REQUEST['parent_id']));
-	if ($productList->error()) $page->addError($productList->error());
