459a4a65dd73c5e457ebf34a8d1032b7d35301d4
Kevin Hinds <khinds10.work@gmail.com>
2025-12-04 11:12:00 -0500

    tag logic fixed, product, account, content blocks

--- modules/product/default/admin_product_tags_mc.php
+++ modules/product/default/admin_product_tags_mc.php
@@ -50,8 +50,8 @@
 				$searchTag->validName($_REQUEST['newSearchTag']) && 
 				$searchTag->validName($_REQUEST['newSearchTagCategory'])) {
 
-				// Check if the tag already exists
-				$existingTag = $searchTagList->find(array('class' => 'Product::Item', 'value' => $_REQUEST['newSearchTag']));
+				// Check if the tag already exists (by both category and value)
+				$existingTag = $searchTagList->find(array('class' => 'Product::Item', 'category' => $_REQUEST['newSearchTagCategory'], 'value' => $_REQUEST['newSearchTag']));
 
 				if (empty($existingTag)) {
 					// Create a new tag
