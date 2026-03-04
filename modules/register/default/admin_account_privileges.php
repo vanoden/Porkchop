6e5b4e2d3b2ae6b4f460b2ac75b1db9076a11703
Anthony Caravello <tony@caravello.us>
2026-03-04 16:03:19 -0500

    Added Account Priveges report

---
+++ modules/register/default/admin_account_privileges.php
@@ -0,0 +1,45 @@
+<?=$page->showSubHeading()?>
+
+<?php $activeTab = 'privileges'; ?>
+
+<div class="tabs">
+    <a href="/_register/admin_account?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='login'?'active':'' ?>">Login / Registration</a>
+    <a href="/_register/admin_account_contacts?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='contacts'?'active':'' ?>">Methods of Contact</a>
+    <a href="/_register/admin_account_password?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='password'?'active':'' ?>">Change Password</a>
+    <a href="/_register/admin_account_roles?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='roles'?'active':'' ?>">Assigned Roles</a>
+    <a href="/_register/admin_account_auth_failures?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='auth_failures'?'active':'' ?>">Recent Auth Failures</a>
+    <a href="/_register/admin_account_terms?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='terms'?'active':'' ?>">Terms of Use History</a>
+    <a href="/_register/admin_account_locations?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='locations'?'active':'' ?>">Locations</a>
+    <a href="/_register/admin_account_images?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='images'?'active':'' ?>">User Images</a>
+    <a href="/_register/admin_account_backup_codes?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='backup_codes'?'active':'' ?>">Backup Codes</a>
+    <a href="/_register/admin_account_search_tags?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='search_tags'?'active':'' ?>">Search Tags</a>
+    <a href="/_register/admin_account_audit_log?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='audit'?'active':'' ?>">Audit Log</a>
+    <a href="/_register/admin_account_register_audit?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='register_audit'?'active':'' ?>">Failed Logins</a>
+	<a href="/_register/admin_account_privileges?customer_id=<?= $customer_id ?>" class="tab <?= $activeTab==='privileges'?'active':'' ?>">Assigned Privileges</a>
+</div>
+<h3><?="Privileges assigned to " . $customer->first_name . " " . $customer->last_name?></h3>
+<div class="tableBody">
+	<div class="tableRowHeader">
+		<div class="tableCell">Role</div>
+		<div class="tableCell">Level</div>
+		<div class="tableCell">Privilege</div>
+	</div>
+	<?php
+	foreach ($privileges as $privilege) {
+	?>
+	<div class="tableRow">
+		<div class="tableCell"><?=$privilege['role']?></div>
+		<div class="tableCell"><?=$privilege['level']?></div>
+		<div class="tableCell"><?=$privilege['privilege']?></div>
+	</div>
+	<?php
+	}
+	if (!is_array($privileges) || !count($privileges)) {
+	?>
+	<div class="tableRow">
+		<div class="tableCell"><p>No Privileges Assigned</p></div>
+	</div>
+	<?php
+	}
+	?>
+</div>
---
+++ modules/register/default/admin_account_privileges_mc.php
@@ -0,0 +1,59 @@
+<?php
+	/** @view /_register/admin_privileges_assigned
+	 * This view is used to display the privileges assigned to a specific customer. It allows administrators to see which privileges a customer has and manage them accordingly.
+	 */
+	$porkchop = new \Porkchop();
+	$site = $porkchop->site();
+	$page = $site->page();
+
+	// See if the user specificed a customer to show
+	if (!empty($_REQUEST['customer_id']) && is_numeric($_REQUEST['customer_id'])) {
+		$customer = new \Register\Customer($_REQUEST['customer_id']);
+		if (!$customer->exists()) {
+			$page->addError("Customer not found");
+			$page->notFound();
+		}
+	}
+	elseif ($GLOBALS['_REQUEST_']->query_vars_array && count($GLOBALS['_REQUEST_']->query_vars_array) > 0 && !empty($GLOBALS['_REQUEST_']->query_vars_array[0])) {
+		$customer = new \Register\Customer();
+		if (!$customer->get($GLOBALS['_REQUEST_']->query_vars_array[0])) {
+			$page->addError("Customer not found");
+			$page->notFound();
+		}
+	}
+	else {
+		$customer = $GLOBALS['_SESSION_']->customer();
+	}
+
+	if (! $GLOBALS['_SESSION_']->customer()->can('manage customers',\Register\PrivilegeLevel::ADMINISTRATOR)) {
+		if (! $GLOBALS['_SESSION_']->customer()->can('manage customers',\Register\PrivilegeLevel::ORGANIZATION_MANAGER)) {
+			$page->permissionDenied();
+		}
+		else {
+			// Organization Managers can only view privileges for customers in their organization
+			if ($customer->organization_id != $GLOBALS['_SESSION_']->customer()->organization_id) {
+				$page->permissionDenied();
+			}
+		}
+	}
+	
+	$roleList = new \Register\RoleList();
+	$roles = $roleList->find();
+
+	$privileges = [];
+	foreach ($roles as $role) {
+		#$privilege["role"] = $role;
+		foreach (\Register\PrivilegeLevel::LEVEL_NAMES as $level => $levelName) {
+			#$privilege["level"] = $levelName;
+			foreach ($role->privileges() as $privilege) {
+				if ($customer->has_privilege($privilege->name,$level)) {
+					array_push($privileges,["role" => $role->name, "level" => $levelName, "privilege" => $privilege->name]);
+				}
+				else {
+					//echo "Customer does NOT have level: " . $levelName . "<br>\n";
+				}
+			}
+		}
+	}
+
+	$page->title("Privilege Assignment Report");
