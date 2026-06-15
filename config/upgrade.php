f9d86f6d007030c8980ec938083df2af3f209d31
Anthony Caravello <tony@caravello.us>
2026-06-15 14:05:52 -0400

    Better organized templates, added portal template

--- config/upgrade.php
+++ config/upgrade.php
@@ -7,7 +7,8 @@
 	$templates = array(
 		"default"	=> "default.html",
 		"support"	=> "support.html",
-		"admin"		=> "admin.html"
+		"admin"		=> "admin.html",
+		"portal"	=> "index_pork.html"
 	);
 
 	// Configuration for each standard Porkchop module
@@ -47,6 +48,7 @@
 				"audit_log"		=> $templates['admin'],
 				"header"		=> $templates['admin'],
 				"headers"		=> $templates['admin'],
+				"messages"		=> $templates['portal'],
 			),
 		),
 		"Geography"		=> array(
@@ -78,6 +80,9 @@
 				"see register api"
 			),
 			"templates"		=> array(
+				"accounts"				=> $templates['support'],
+				"account"				=> $templates['portal'],
+				"admin_account"		=> $templates['admin'],
 				"organizations_report"	=> $templates['admin'],
 				"admin_organizations"	=> $templates['admin'],
 				"admin_organization"		=> $templates['admin'],
@@ -85,11 +90,6 @@
 				"admin_organization_tags"	=> $templates['admin'],
 				"admin_organization_locations"	=> $templates['admin'],
 				"admin_organization_audit_log"	=> $templates['admin'],
-				"admin_account"		=> $templates['admin'],
-				"pending_customers"	=> $templates['admin'],
-				"privileges"		=> $templates['admin'],
-				"roles"				=> $templates['admin'],
-				"role"				=> $templates['admin'],
 				"admin_location"	=> $templates['admin'],
 				"admin_accounts"		=> $templates['admin'],
 				"admin_account_contacts"	=> $templates['admin'],
@@ -103,12 +103,15 @@
 				"admin_account_search_tags"	=> $templates['admin'],
 				"admin_account_audit_log"	=> $templates['admin'],
 				"admin_account_register_audit"	=> $templates['admin'],
-				"ent_accounts"			=> $templates['support'],
 				"admin_account_privileges"	=> $templates['admin'],
 				"admin_organization_plans"	=> $templates['admin'],
+				"pending_customers"	=> $templates['admin'],
+				"privileges"		=> $templates['admin'],
+				"roles"				=> $templates['admin'],
+				"role"				=> $templates['admin'],
+				"ent_accounts"			=> $templates['support'],
 				"organization"				=> $templates['support'],
 				"organizations"			=> $templates['support'],
-				"accounts"				=> $templates['support']
 			),
 		),
 		"Contact"		=> array(
@@ -136,61 +139,25 @@
 		"Search"		=> array(
 			"schema"	=> 3
 		),
-		"Engineering"	=> array(
-			"templates"	=> array(
-				"home"			=> $templates['admin'],
-				"tasks"			=> $templates['admin'],
-				"task"			=> $templates['admin'],
-				"releases"		=> $templates['admin'],
-				"release"		=> $templates['admin'],
-				"projects"		=> $templates['admin'],
-				"project"		=> $templates['admin'],
-				"products"		=> $templates['admin'],
-				"product"		=> $templates['admin'],
-				"event_report"	=> $templates['admin'],
-				"search"		=> $templates['admin'],
-			)
-		),
-		"Support"		=> array(
-			"templates"	=> array(
-				"request_new"	=> $templates['admin'],
-				"requests"		=> $templates['admin'],
-				"request_detail"	=> $templates['admin'],
-				"request_items"	=> $templates['admin'],
-				"request_item"	=> $templates['admin'],
-				"action"		=> $templates['admin'],
-				"admin_actions"	=> $templates['admin'],
-				"summary"		=> $templates['admin'],
-				"admin_rmas"	=> $templates['admin'],
-				"admin_rma"		=> $templates['admin'],
-			)
-		),
-		"Monitor"		=> array(
-			"templates"	=> array(
-				"admin_assets"	=> $templates['admin'],
-				"admin_details"	=> $templates['admin'],
-				"admin_collections"	=> $templates['admin'],
-				"comm_dashboard"	=> $templates['admin'],
-				"sensor_models"	=> $templates['admin'],
-				"sensor_model"	=> $templates['admin'],
-				"dashboards"	=> $templates['admin'],
-				"admin_dashboard"	=> $templates['admin'],
-			)
-		),
 	);
 
-	// Additional modules
-	include(MODULES."/product/default/_metadata.php");
-	include(MODULES."/sales/default/_metadata.php");
-	include(MODULES."/network/default/_metadata.php");
-	include(MODULES."/storage/default/_metadata.php");
-	include(MODULES."/shipping/default/_metadata.php");
-	include(MODULES."/package/default/_metadata.php");
-	include(MODULES."/support/default/_metadata.php");
-	include(MODULES."/form/default/_metadata.php");
+	// Individual module overrides (e.g. for custom templates or privileges)
+	$module_folders = scandir(MODULES);
+	foreach ($module_folders as $folder) {
+		if (substr($folder,0,1) != "." && is_dir(MODULES."/".$folder)) {
+			if (!empty($GLOBALS['_config']->style[$folder])) {
+				$style = $GLOBALS['_config']->style[$folder];
+			} else {
+				$style = "default";
+			}
+			if (file_exists(MODULES."/".$folder."/".$style."/_metadata.php")) {
+				include(MODULES."/".$folder."/".$style."/_metadata.php");
+			}
+		}
+	}
 
 	$menus = array();
-	// Include upgrade.local.php from any module that has one (e.g. site, spectros)
+	// Include Site Specific overrides with upgrade.local.php from any module that has one (e.g. site, spectros)
 	foreach (array_keys($modules) as $module) {
 		$path = MODULES."/".strtolower($module)."/default/upgrade.local.php";
 		if (file_exists($path)) {
